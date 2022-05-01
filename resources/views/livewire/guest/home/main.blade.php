736661aa1c504be1e59492b0a2ce21f809a7475b
zal3 <zainabamjedlatef31@gmail.com>
2022-05-02 01:53:13 +0300

    aa

--- resources/views/livewire/guest/home/main.blade.php
+++ resources/views/livewire/guest/home/main.blade.php
@@ -2,13 +2,13 @@
     <div class="bg-white flex p-10  ">
 
         <a  class=" mt-8 mr-14 ml-14 p-6 max-w-full  ">
-            <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">نحن نعيش </h6>
+            <h6 class="mb-2 text-3xl tracking-tight text-gray-900 dark:text-white">نحن نعيش </h6>
             <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">السعادة عبر</h1>
             <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">العطاء </h1>
-            <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">لمن هو محتاج </h6>
+            <h6 class="mb-2 text-3xl  tracking-tight text-gray-900 dark:text-white">لمن هو محتاج </h6>
 
             <div class="flex  item-center mt-7 text-2xl ">
-                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-100 "> أشتراك<br> يومي </div>
+                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-100 "> أشتراك<br> يومي </div>
                 <div  class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-300"> اشتراك  <br>اسبوعي </div>
                 <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-300"> أشتراك<br> شهري</div>
             </div>
