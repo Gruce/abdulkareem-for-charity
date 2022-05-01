cefd1fd260f8c1c20c3bb57a1afc113671dcb613
zal3 <zainabamjedlatef31@gmail.com>
2022-05-02 01:20:27 +0300

    add

--- resources/views/livewire/guest/home/main.blade.php
+++ resources/views/livewire/guest/home/main.blade.php
@@ -1,28 +1,28 @@
 <div>
-    <div class="bg-white flex p-10 justify-center ">
+    <div class="bg-white flex p-10  ">
 
-        <div class=" mt-8 ml-10 p-6 max-w-lg bg-white">
+        <a  class=" mt-8 mr-14 ml-14 p-6 max-w-full  ">
             <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">نحن نعيش </h6>
             <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">السعادة عبر</h1>
             <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">العطاء </h1>
             <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">لمن هو محتاج </h6>
 
-            <div class="flex  justify-between mt-7 text-2xl ">
-                <div class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-400 "> أشتراك<br> يومي </div>
-                <div  class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300"> اشتراك  <br>اسبوعي </div>
-                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300"> أشتراك<br> شهري</div>
+            <div class="flex  item-center mt-7 text-2xl ">
+                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-100 "> أشتراك<br> يومي </div>
+                <div  class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-300"> اشتراك  <br>اسبوعي </div>
+                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-300"> أشتراك<br> شهري</div>
             </div>
 
-        </div>
+        </a>
 
-        <div class="mt-8 mr-10 p-6 max-w-lg bg-white ">
-            <img class="rounded-t-lg"
+        <a class="mt-8 mr-14    item-center   ">
+            <img class="rounded-t-lg max-w-2xl"
                 src="https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g"
                 alt="photo" />
             <p class=" mt-4 text-sm text-center font-normal tracking-tight text-gray-900 dark:text-white">
                 مابعرفششششششششششششششش </p>
 
-        </div>
+        </a>
 
     </div>
 </div>
