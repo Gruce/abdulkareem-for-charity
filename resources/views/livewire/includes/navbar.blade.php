ad3f96e2fcd8cef0c8f83f14d1b3834461b237e3
Mosa Nameer <mosa.mn777@gmail.com>
2022-05-01 23:58:42 +0300

    add

--- resources/views/livewire/includes/navbar.blade.php
+++ resources/views/livewire/includes/navbar.blade.php
@@ -1,7 +1,7 @@
-<nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+<nav class="bg-gray-300 border-gray-200 px-2 py-4 sm:px-4 rounded">
     <div class="container flex flex-wrap items-center justify-between mx-auto">
         <a href="https://flowbite.com" class="flex items-center">
-            <img src="/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo">
+            <img src="/docs/images/logo.svg" class="h-6 mr-3 sm:h-9" alt="">
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
 
