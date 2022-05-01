ed321a3f1cc9994865654f582ae33f5e0e626191
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-02 01:19:17 +0300

    edit

--- resources/views/livewire/guest/home/main.blade.php
+++ resources/views/livewire/guest/home/main.blade.php
@@ -1,16 +1,21 @@
 <div>
     <div class="bg-white flex p-10 justify-center ">
 
-        <div class=" mt-8 ml-10 p-6 max-w-lg bg-white">
+        <div class=" mt-8 ml-20 p-6 max-w-lg bg-white">
+            <p class=" mt-4 text-sm text-center font-normal tracking-tight text-red-300 dark:text-white">
+                بينما يعود النهر الى البحر, تعود عطايا الانسان اليه. </p>
             <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">نحن نعيش </h6>
             <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">السعادة عبر</h1>
             <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">العطاء </h1>
             <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">لمن هو محتاج </h6>
 
-            <div class="flex  justify-between mt-7 text-2xl ">
-                <div class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-400 "> أشتراك<br> يومي </div>
-                <div  class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300"> اشتراك  <br>اسبوعي </div>
-                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300"> أشتراك<br> شهري</div>
+            <div class="flex justify-between mt-7 text-2xl ">
+                <div class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300 ">
+                    أشتراك<br> يومي </div>
+                <div class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300"> اشتراك
+                    <br>اسبوعي </div>
+                <div class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-green-300">
+                    أشتراك<br> شهري</div>
             </div>
 
         </div>
@@ -20,7 +25,7 @@
                 src="https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g"
                 alt="photo" />
             <p class=" mt-4 text-sm text-center font-normal tracking-tight text-gray-900 dark:text-white">
-                مابعرفششششششششششششششش </p>
+                وصف الصورة </p>
 
         </div>
 
