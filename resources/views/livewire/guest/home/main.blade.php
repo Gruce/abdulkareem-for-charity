83a1e8ccb78af8656bea836f08b10c0579746ebd
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-02 02:16:57 +0300

    edit

--- resources/views/livewire/guest/home/main.blade.php
+++ resources/views/livewire/guest/home/main.blade.php
@@ -1,28 +1,63 @@
 <div>
     <div class="bg-white flex p-10  ">
 
-        <a  class=" mt-8 mr-14 ml-14 p-6 max-w-full  ">
-            <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">نحن نعيش </h6>
-            <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">السعادة عبر</h1>
-            <h1 class="mb-2 text-6xl font-bold tracking-tight text-gray-900 dark:text-white">العطاء </h1>
-            <h6 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">لمن هو محتاج </h6>
+        <div class=" mt-8 mr-14 ml-14 p-6 max-w-full">
+            <p class=" mt-4  text-right tracking-tight text-primary-100 dark:text-white">
+                <i class="text-3xl font-bold  fa-solid fa-hand-holding-hand"></i>
+            </p>
+            <h6 class="mb-2 text-3xl font-bold tracking-tight text-primary-100 dark:text-white">نحن نعيش </h6>
+            <h1 class="mb-2 text-6xl font-bold tracking-tight text-primary-100 dark:text-white">السعادة عبر</h1>
+            <h1 class="mb-2 text-6xl font-bold tracking-tight text-primary-100 dark:text-white">العطاء </h1>
+            <h6 class="mb-2 text-3xl font-bold tracking-tight text-primary-100 dark:text-white">لمن هو محتاج </h6>
 
-            <div class="flex  item-center mt-7 text-2xl ">
-                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-100 "> أشتراك<br> يومي </div>
-                <div  class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-300"> اشتراك  <br>اسبوعي </div>
-                <div  class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-300"> أشتراك<br> شهري</div>
+            <div class="flex item-center mt-10 text-xl ">
+                <div class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-100 ">
+                    أشتراك<br> يومي </div>
+                <div class=" pt-7 pb-7 pr-2 pl-2 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-100"> اشتراك
+                    <br>اسبوعي
+                </div>
+                <div class=" pt-7 pb-7 pr-3 pl-3 ml-2 mr-2 rounded-lg border border-gray-400 hover:bg-blue-100">
+                    أشتراك<br> شهري</div>
             </div>
 
-        </a>
+        </div>
 
-        <a class="mt-8 mr-14    item-center   ">
+        <div class="mt-8 mr-14 item-center ">
             <img class="rounded-t-lg max-w-2xl"
                 src="https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g"
                 alt="photo" />
-            <p class=" mt-4 text-sm text-center font-normal tracking-tight text-gray-900 dark:text-white">
-                مابعرفششششششششششششششش </p>
-
-        </a>
+            <p class=" mt-4 text-sm text-center font-normal tracking-tight text-secondary-100 dark:text-white">
+                وصف الصورة </p>
+        </div>
 
     </div>
+    <div class="flex justify-center mt-4 ">
+        <div
+            class="w-full mr-4 p-6 max-w-sm bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <i class="text-2xl text-secondary-100 fa-solid fa-circle-dollar-to-slot"></i>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-secondary-100 dark:text-white">500$</h5>
+            </a>
+
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">عدد التبرعات الكلي.</p>
+        </div>
+
+        <div
+            class="w-full mr-4 p-6 max-w-sm bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <i class="text-2xl text-secondary-100 fa-solid fa-circle-dollar-to-slot"></i>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-secondary-100 dark:text-white">500$</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">عدد التبرعات الكلي.</p>
+        </div>
+        <div
+            class="w-full mr-4 p-6 max-w-sm bg-white rounded-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <i class="text-2xl text-secondary-100 fa-solid fa-circle-dollar-to-slot"></i>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-secondary-100 dark:text-white">500$</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">عدد التبرعات الكلي.</p>
+        </div>
+    </div>
+
 </div>
