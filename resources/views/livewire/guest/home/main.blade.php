2aa1c33385dbc2c41b0975d473a0f12e9efacfdb
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-02 06:37:03 +0300

    Update main.blade.php

--- resources/views/livewire/guest/home/main.blade.php
+++ resources/views/livewire/guest/home/main.blade.php
@@ -1,16 +1,16 @@
 <div>
-    <div class="bg-white grid grid-cols-2 py-6  pb-10">
+    <div class="bg-white grid grid-cols-2 py-6 pb-10">
         <div class=" mt-8 mr-14 ml-14 p-7 max-w-full ">
-            <p class=" mt-4  text-right tracking-tight text-primary-100 dark:text-white">
-                <i class="text-3xl font-bold  fa-solid fa-hand-holding-hand"></i>
+            <p class=" mt-4 tracking-tight text-primary-100 dark:text-white">
+                <i class="text-3xl font-bold fa-solid fa-hand-holding-hand"></i>
             </p>
             <h6 class="mb-2 text-3xl font-bold tracking-tight text-primary-100 dark:text-white">نحن نعيش </h6>
             <h1 class="mb-2 text-5xl font-bold tracking-tight text-primary-100 dark:text-white">السعادة عبر</h1>
             <h1 class="mb-2 text-5xl font-bold tracking-tight text-primary-100 dark:text-white">العطاء </h1>
             <h6 class="mb-2 text-3xl font-bold tracking-tight text-primary-100 dark:text-white">لمن هو محتاج </h6>
 
-            <div class="flex item-center mt-10">
-                <button class="btn btn-lg rounded-lg border border-gray-600 hover:bg-blue-100 mr-1 ml-1 p-6">
+            <div class="flex item-center mt-10 ">
+                <button class="btn btn-lg text-center rounded-lg border border-gray-600 hover:bg-blue-100 mr-1 ml-1 p-6">
                     <a href="#" class=" ">
                         اشتراك يومي
                     </a>
@@ -30,12 +30,12 @@
         </div>
         <div class="mt-8 mr-14 item-center ">
             <img class="rounded-lg max-w-2xl"
-                src="https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g"
+                src="https://i.picsum.photos/id/106/2592/1728.jpg?hmac=E1-3Hac5ffuCVwYwexdHImxbMFRsv83exZ2EhlYxkgY"
                 alt="photo" />
         </div>
     </div>
 
-    <div class="contener rounded-lg grid-cols-3  gap-4  bg-gray-50  flex justify-center  my-10">
+    <div class="contener rounded-lg grid-cols-3 gap-4  bg-gray-50 flex justify-center  my-10">
 
         <div class="p-6 text-center dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
@@ -44,14 +44,14 @@
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-xl">عدد التبرعات الكلي.</p>
         </div>
-        <div class="p-6 text-center  dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-6 text-center dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
                 <i class="fa-solid fa-handshake-simple text-3xl mb-3"></i>
                 <h5 class="mb-2 text-3xl font-bold tracking-tight  dark:text-white">66</h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 text-xl">عدد المتبرعين.</p>
         </div>
-        <div class=" p-6  text-center  dark:bg-gray-800 dark:border-gray-700">
+        <div class=" p-6  text-center dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
                 <i class="fa-solid fa-hand-holding-heart text-3xl mb-3"></i>
                 <h5 class="mb-2 text-3xl font-bold tracking-tight dark:text-white">33</h5>
