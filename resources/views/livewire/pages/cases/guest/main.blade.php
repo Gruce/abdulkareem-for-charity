ce45bf720147600703803c31476a13f742d21ed9
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-05-04 23:38:16 +0300

    add

--- resources/views/livewire/pages/cases/guest/main.blade.php
+++ resources/views/livewire/pages/cases/guest/main.blade.php
@@ -1,4 +1,4 @@
-<div class="grid  gap-6  xs:grid-cols-1  lg:grid-cols-3 xl:grid-cols-3">
+<div class="mt-3 grid  gap-6  xs:grid-cols-1  lg:grid-cols-3 xl:grid-cols-3">
     @for($i=0;$i<=2;$i++)
         <livewire:components.case.card/>
     @endfor
