28860a39417f708855064d4a71c5df3cd9bb5535
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-07 18:48:18 +0300

    add

---
+++ app/Http/Livewire/Pages/Cases/Admin/Add.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Cases\Admin;
+
+use Livewire\Component;
+
+class Add extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.cases.admin.add');
+    }
+}
--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -88,7 +88,7 @@
         </div>
         @admin 
         <div>
-            <button type="button" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 ">Add Case</button>
+            <button type="button" wire:click="add" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 ">Add Case</button>
 
         </div>
         @endadmin
