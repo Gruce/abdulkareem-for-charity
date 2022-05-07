fafe406df0fd5e633ff14691332c9859063c6505
Ahmed <mr.darkboss1@gmail.com>
2022-05-08 01:58:57 +0300

    Button

---
+++ app/View/Components/UI/Button.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\View\Components\UI;
+
+use Illuminate\View\Component;
+
+class Button extends Component
+{
+    public $color;
+    public $default;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($color = "primary")
+    {
+        $this->default = 'text-white focus:ring-2 font-medium text-sm rounded-lg px-5 py-2.5 focus:outline-none hover:scale-105 duration-200 cursor-pointer ';
+
+        if ($this->color = $color) {
+
+            switch ($this->color) {
+                case "primary":
+                    $this->default .= 'bg-blue-500 hover:bg-blue-800 focus:ring-blue-300';
+                    break;
+                case "secondary":
+                    $this->default .= 'bg-gray-700 hover:bg-gray-800 focus:ring-gray-300';
+                    break;
+                case "success":
+                    $this->default .= 'bg-green-700 hover:bg-green-800 focus:ring-green-300';
+                    break;
+                case "error":
+                    $this->default .= 'bg-red-700 hover:bg-red-800 focus:ring-red-300';
+                    break;
+                case "warning":
+                    $this->default .= 'bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-200';
+                    break;
+                case "light":
+                    $this->default .= 'text-white bg-gray-600 border-gray-700 hover:bg-gray-900 focus:ring-gray-300';
+                    break;
+                case "light2":
+                    $this->default .= 'text-gray-200 bg-gray-400 border-gray-600 hover:bg-gray-700 focus:ring-gray-300';
+                    break;
+            }
+        } else
+            $this->default .= 'bg-red-500 hover:bg-red-800 focus:ring-red-300';
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.u-i.button');
+    }
+}
