1101827e93723c13eb1cc39763aaa3f816139540
Mosa Nameer <mosa.mn777@gmail.com>
2022-05-07 18:29:22 +0300

    add

--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -95,7 +95,8 @@
         
         @auth
         <div>
-            تسجيل خروج
+            <a href="{{route('logout')}}" class="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 ">تسجيل خروج</a>
+
         </div>
         @endauth
 
