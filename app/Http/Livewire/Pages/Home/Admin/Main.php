4c64683ac94268633f3b864bee90e41f63b55527
Mohammed Jawad <jawad6686@gmail.com>
2022-05-07 17:55:03 +0300

    add

---
+++ app/Http/Livewire/Pages/Home/Admin/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Home\Admin;
+
+use Livewire\Component;
+
+class Main extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.home.admin.main');
+    }
+}
