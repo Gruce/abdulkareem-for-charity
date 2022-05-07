91294838f49a99de44aa5429a52e5bf3911d0208
Hassan Alkhalidi <gruceing@gmail.com>
2022-05-07 23:47:10 +0300

    asd

--- resources/views/livewire/pages/cases/admin/add.blade.php
+++ resources/views/livewire/pages/cases/admin/add.blade.php
@@ -1,10 +1,10 @@
 {{-- <div>
-    <div class="basis-5/6 w-5/6">
+    <div class="w-5/6 basis-5/6">
         <form wire:submit.prevent="add">
             {{-- name --
             <div class="grid grid-cols-none gap-1">
                 <div class="mb-6">
-                    <label for="name" class=" block mb-2 text-sm font-medium text-gray-900 ">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">
                         اسم الحالة
 
                     </label>
@@ -15,7 +15,7 @@
                 </div>
                 {{-- image --
                 <div class="mb-6">
-                    <label for="name" class=" block mb-2 text-sm font-medium text-gray-900">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">
                         صورة الحالة
                     </label>
                     <input required wire:model.lazy="image_path" type="file" id="name"
@@ -25,7 +25,7 @@
                 </div>
                 {{-- details file --
                 <div class="mb-6">
-                    <label for="name" class=" block mb-2 text-sm font-medium text-gray-900">
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">
                         مستمسكات شخصية
 
                     </label>
@@ -36,7 +36,7 @@
                 </div>
                 {{-- target --
                 <div class="mb-6">
-                    <label for="الهدف" class=" block mb-2 text-sm font-medium text-gray-900">
+                    <label for="الهدف" class="block mb-2 text-sm font-medium text-gray-900 ">
                         الهدف
 
                     </label>
@@ -47,7 +47,7 @@
                 </div>
                 {{-- received_price --}}
                 {{-- <div class="mb-6">
-                <label for="name" class=" block mb-2 text-sm font-medium text-gray-900">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">
                 تم دفعه
 
                 </label>
@@ -58,8 +58,8 @@
             </div> --}}
 
                 {{-- description --
-                <div class="mt-4 grid grid-cols-1 gap-4 ">
-                    <label for="message" class=" block text-sm font-medium text-gray-900">
+                <div class="grid grid-cols-1 gap-4 mt-4 ">
+                    <label for="message" class="block text-sm font-medium text-gray-900 ">
                         الوصف
 
                     </label>
@@ -80,28 +80,28 @@
 
 <!-- component -->
 <div class="">
-<div class="flex h-screen items-center justify-center  mt-32 mb-32">
-    <div class="grid  rounded-lg  w-full ">
+<div class="flex items-center justify-center h-screen mt-32 mb-32">
+    <div class="grid w-full rounded-lg ">
       <div class="flex justify-center py-4">
-        <div class="flex bg-green-200 rounded-full md:p-4 p-2 border-2 border-blue-300">
+        <div class="flex p-2 bg-green-200 border-2 border-blue-300 rounded-full md:p-4">
           <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
         </div>
       </div>
   
       <div class="flex justify-center">
         <div class="flex">
-          <h1 class="text-gray-800 font-bold md:text-2xl text-xl">إضافة حالات</h1>
+          <h1 class="text-xl font-bold text-gray-800 md:text-2xl">إضافة حالات</h1>
         </div>
       </div>
   
       <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-700 text-light font-semibold">عنوان الحالة</label>
-        <input class="py-2 px-3 rounded-lg border-2 border-primary-600 mt-1 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:border-transparent" type="text" />
+        <label class="text-xs font-semibold text-gray-700 uppercase md:text-sm text-light">عنوان الحالة</label>
+        <input class="px-3 py-2 mt-1 border-2 rounded-lg border-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-600 focus:border-transparent" type="text" />
       </div>
   
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
+      <div class="grid grid-cols-1 gap-5 mt-5 md:grid-cols-2 md:gap-8 mx-7">
         <div class="grid grid-cols-1">
-            <label for="message" class="block text-sm text-gray-700 text-light font-semibold">
+            <label for="message" class="block text-sm font-semibold text-gray-700 text-light">
                 الوصف
             </label>
             <textarea required wire:model.lazy="description" id="message" rows="4"
@@ -110,31 +110,31 @@
         </div>
 
         <div class="grid grid-cols-1">
-          <label class="uppercase md:text-sm text-xs text-gray-700 text-light font-semibold">المبلغ الكلي</label>
-          <input class="py-2 px-3 rounded-lg border-2 border-blue-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" />
+          <label class="text-xs font-semibold text-gray-700 uppercase md:text-sm text-light">المبلغ الكلي</label>
+          <input class="px-3 py-2 mt-1 border-2 border-blue-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" />
         </div>
       </div>
   
       <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-700 text-light font-semibold">المبلغ الواصل</label>
-        <input class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text"/>
+        <label class="text-xs font-semibold text-gray-700 uppercase md:text-sm text-light">المبلغ الواصل</label>
+        <input class="px-3 py-2 mt-1 border-2 border-purple-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text"/>
       </div>
   
       <div class="grid grid-cols-1 mt-5 mx-7">
-        <label class="uppercase md:text-sm text-xs text-gray-700 text-light font-semibold mb-1">ارفاق صورة</label>
+        <label class="mb-1 text-xs font-semibold text-gray-700 uppercase md:text-sm text-light">ارفاق صورة</label>
           <div class='flex items-center justify-center w-full'>
-              <label class='flex flex-col border-4 border-dashed border-blue-100 w-full h-32 hover:bg-blue-100 hover:border-blue-300 group rounded-lg'>
+              <label class='flex flex-col w-full h-32 border-4 border-blue-100 border-dashed rounded-lg hover:bg-blue-100 hover:border-blue-300 group'>
                   <div class='flex flex-col items-center justify-center pt-7'>
                     <svg class="w-10 h-10 text-gray-400 group-hover:text-blue-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                    <p class='lowercase text-sm text-gray-400 group-hover:text-gray-600 pt-1 tracking-wider'>اختر صورة</p>
+                    <p class='pt-1 text-sm tracking-wider text-gray-400 lowercase group-hover:text-gray-600'>اختر صورة</p>
                   </div>
                 <input type='file' class="hidden" />
               </label>
           </div>
       </div>
   
-      <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-        <button class='w-auto bg-blue-500 hover:bg-blue-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Create</button>
+      <div class='flex items-center justify-center gap-4 pt-5 pb-5 md:gap-8'>
+        <button class='w-auto px-4 py-2 font-medium text-white bg-blue-500 rounded-lg shadow-xl hover:bg-blue-700'>Create</button>
       </div>
   
     </div>
