55a313da96d1963047e33e220afb9a2d0a2240fc
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-09 01:50:28 +0300

    Update Main.php

--- app/Http/Livewire/Pages/Donate/Main.php
+++ app/Http/Livewire/Pages/Donate/Main.php
@@ -4,15 +4,24 @@ namespace App\Http\Livewire\Pages\Donate;
 
 use Livewire\Component;
 use App\Models\Share;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Main extends Component
 {
+    use LivewireAlert;
     public $share, $note;
 
     public function save()
     {
         $donate = new Share;
         $donate->add($this->share, $this->note);
+
+        $this->alert('success', 'Done!', [
+            'position' => 'top',
+            'timer' => 3000,
+            'toast' => true,
+        ]);  
+        $this->reset();
     }
 
     public function render()
