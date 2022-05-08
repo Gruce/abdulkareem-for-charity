c8841b13f2e0b6a1910cf47d249c00e8b0502be7
Mohammed Jawad <jawad6686@gmail.com>
2022-05-08 10:21:39 +0300

    a

--- resources/views/livewire/pages/profile/student-info.blade.php
+++ resources/views/livewire/pages/profile/student-info.blade.php
@@ -30,6 +30,8 @@
 
 
 
+                
+
                 <div class="form-item w-full">
                     <label class="text-xl ">المرحلة</label>
                     <select  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
