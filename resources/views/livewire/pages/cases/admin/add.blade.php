f42c9aa21774b5a1b53059634534e95c656779f6
Mohammed Jawad <jawad6686@gmail.com>
2022-05-08 11:46:10 +0300

    Update add.blade.php

--- resources/views/livewire/pages/cases/admin/add.blade.php
+++ resources/views/livewire/pages/cases/admin/add.blade.php
@@ -76,7 +76,7 @@
                             <p class="text-sm text-gray-400 group-hover:text-gray-600 pt-1 tracking-wider">
                                 اختر صورة</p>
                         </div>
-                        <input type="file" class="hidden" />
+                        <input wire:model.lazy="file_path" type="file" class="hidden" />
                     </label>
                 </div>
             </div>
