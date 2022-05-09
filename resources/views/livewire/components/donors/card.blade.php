55854477355a94148fa1f0efee7b68ce36bfef74
Mohammed Jawad <jawad6686@gmail.com>
2022-05-09 16:12:32 +0300

    add

--- resources/views/livewire/components/donors/card.blade.php
+++ resources/views/livewire/components/donors/card.blade.php
@@ -1,28 +1,17 @@
 <div>
 
-    {{-- <div
-        class="relative group bg-secondary-500 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-secondary-800/80 hover:smooth-hover">
-        <img class="w-20 h-20 object-cover object-center rounded-full"
-            src="https://images.unsplash.com/photo-1513364776144-60967b0f800f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
-            alt="art" />
-        <h4 class="text-white text-2xl font-bold capitalize text-center">Art</h4>
-        <p class="text-white/50">132 members</p>
-        <p class="absolute top-2 text-white/20 inline-flex items-center text-xs">4 Online <span
-                class="ml-2 w-2 h-2 block bg-green-500 rounded-full group-hover:animate-pulse"></span></p>
-    </div> --}}
-
     <div>
         <div class=" p-4 w-full rounded-t-xl bg-gray-50 hover:bg-gray-100 transition duration-300">
             <div class="grid grid-cols-5 mb-5">
-                <div class="col-span-2 flex flex-col justify-center">
+                <div class="col-span-2 flex flex-col">
                     <img src="https://images.unsplash.com/photo-1513364776144-60967b0f800f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
                         alt="" class=" rounded-full h-32 w-32" />
-                    <h3 class="text-center text-2xl font-bold">طالب</h3>
+                    <h3 class=" text-xl mr-2 mt-2"> يملك 20 سهما </h3>
                 </div>
 
                 <div class="col-span-3">
                     <h3 class="mt-6 text-center text-2xl ">محمد جواد كاظم</h3>
-                    <h3 class="mt-2 text-center text-base ">يملك 20 سهما</h3>
+                    <h3 class="mt-2 text-center text-base ">طالب</h3>
                 </div>
             </div>
             <div class="text-center">
@@ -30,23 +19,31 @@
 
 
             </div>
-            <ul class="mt-8 mb-20 flex justify-center space-x-4 text-center text-lg">
+            <ul class=" flex justify-start space-x-4 text-center text-lg" x-data="{ isOpen: false}">
 
-
-
-
-
-
-                <button
-                    class="mt-2 text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                    Accept
+                <button @click="isOpen = !isOpen"
+                    class="mt-2 ml-10 text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                    زيادة الاسهم
                 </button>
 
+                <form class="flex flex-row-reverse" x-show="isOpen">
+                    <div>
+
+                        <input type="number" id="shares"
+                            class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block h-12 "
+                            placeholder="ادخل عدد الاسهم التي تريد اضافتها">
+                    </div>
+
+
+                    <button type="submit"
+                        class="mt-2 w-full text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">اضافة
+                    </button>
+                </form>
 
 
 
             </ul>
-            <h5 class="text-lg font-bold tracking-tight pt-5">البريد الاكتروني: jawad6686@gmail.com
+            <h5 class="text-lg font-bold tracking-tight pt-5">البريد الالكتروني: jawad6686@gmail.com
             </h5>
             <p class="text-sm font-normal text-gray-500">تاريخ الانضمام: 2022/10/1</p>
 
