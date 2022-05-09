2d2a101c169eebeb18a0f19f2c7bb8fefd071ac7
Hussein Najah <98782046+i1Zeus@users.noreply.github.com>
2022-05-09 23:16:27 +0300

    about

--- resources/views/livewire/pages/about/main.blade.php
+++ resources/views/livewire/pages/about/main.blade.php
@@ -1,10 +1,11 @@
-<x-ui.button >تسجيل دخول</x-ui.button>
-<div>
-    <div class="grid md:grid-cols-10 sm:grid-cols-1 sm:grid-rows-6">
+<div class="p-4">
+    <div class="h-screen grid md:grid-cols-10 sm:grid-cols-1 sm:grid-rows-6">
         <div class="sm:col-span-6 sm:grid-rows-4 mt-10 md:mt-0">
-        
-            <h1 class="text-3xl md:text-5xl font-Kufi text-center font-bold text-primary-600"> صندوق الأستاذ عبدالكريم</h1>
-            <p class="mt-10 text-xl text-gray-700 px-1 md:px-9">"صـندوق الأستاذ عبد الكريم الخيري" هو مبادرة ونشاط طلابي بحت ينطلق من
+
+            <h1 class="text-3xl md:text-5xl font-Kufi text-center font-bold text-primary-600"> صندوق الأستاذ عبدالكريم
+            </h1>
+            <p class="mt-10 text-xl text-gray-700 px-1 md:px-9">"صـندوق الأستاذ عبد الكريم الخيري" هو مبادرة ونشاط طلابي
+                بحت ينطلق من
                 طلبة كلية علوم الحاسوب وتكنلوجيا المعلومات. <br>
 
                 الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق للطلبة ذوي الدخل دون المحدود
@@ -18,11 +19,106 @@
                 2- أمانة الصندوق <br>
                 3- الاعمال التي تُصرف فيها مبالغ الصندوق<br>
             </p>
+           
+            {{-- Carators/Programmers --}}
+            <div class="grid grid-rows-3 gap-5 p-7">
+                <article class="w-64 p-4 bg-gray-800 border border-gray-700 rounded-xl">
+                    <div class="flex justify-between items-center">
+                        <img src="https://unavatar.now.sh/twitter/itsmarkmead" alt="صوره بروف"
+                            class="w-16 h-16 rounded-full" />
+
+                        <div class="ml-3">
+                            <h5 class="text-lg font-medium text-white">حسن الخالدي</h5>
+                            <div class="flow-root">
+                                <ul class="flex flex-wrap -m-1">
+                                    <li class="p-1 leading-none">
+                                        <a href="https://twitter.com/itsmarkmead" target="_blank"
+                                            class="text-xs font-medium text-gray-300">
+                                            Twitter
+                                        </a>
+                                    </li>
+                                    <li class="p-1 leading-none">
+                                        <a href="https://github.com/markmead" target="_blank"
+                                            class="text-xs font-medium text-gray-300">
+                                            GitHub
+                                        </a>
+                                    </li>
+                                    <li class="p-1 leading-none">
+                                        <a href="https://markmead.dev/" target="_blank"
+                                            class="text-xs font-medium text-gray-300">Instagram</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article class="w-64 p-4 bg-gray-800 border border-gray-700 rounded-xl">
+                    <div class="flex justify-between items-center">
+                        <img src="https://unavatar.now.sh/twitter/itsmarkmead" alt="صوره بروف"
+                            class="w-16 h-16 rounded-full" />
+
+                        <div class="ml-3">
+                            <h5 class="text-lg font-medium text-white">حسن الخالدي</h5>
+                            <div class="flow-root">
+                                <ul class="flex flex-wrap -m-1">
+                                    <li class="p-1 leading-none">
+                                        <a href="https://twitter.com/itsmarkmead" target="_blank"
+                                            class="text-xs font-medium text-gray-300">
+                                            Twitter
+                                        </a>
+                                    </li>
+                                    <li class="p-1 leading-none">
+                                        <a href="https://github.com/markmead" target="_blank"
+                                            class="text-xs font-medium text-gray-300">
+                                            GitHub
+                                        </a>
+                                    </li>
+                                    <li class="p-1 leading-none">
+                                        <a href="https://markmead.dev/" target="_blank"
+                                            class="text-xs font-medium text-gray-300">Instagram</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+                <article class="w-64 p-4 bg-gray-800 border border-gray-700 rounded-xl">
+                    <div class="flex justify-between items-center">
+                        <img src="https://unavatar.now.sh/twitter/itsmarkmead" alt="صوره بروف"
+                            class="w-16 h-16 rounded-full" />
+
+                        <div class="ml-3">
+                            <h5 class="text-lg font-medium text-white">حسن الخالدي</h5>
+                            <div class="flow-root">
+                                <ul class="flex flex-wrap -m-1">
+                                    <li class="p-1 leading-none">
+                                        <a href="https://twitter.com/itsmarkmead" target="_blank"
+                                            class="text-xs font-medium text-gray-300">
+                                            Twitter
+                                        </a>
+                                    </li>
+                                    <li class="p-1 leading-none">
+                                        <a href="https://github.com/markmead" target="_blank"
+                                            class="text-xs font-medium text-gray-300">
+                                            GitHub
+                                        </a>
+                                    </li>
+                                    <li class="p-1 leading-none">
+                                        <a href="https://markmead.dev/" target="_blank"
+                                            class="text-xs font-medium text-gray-300">Instagram</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </div>
         </div>
         <div class="md:col-span-4 sm:grid-rows-2 order-first md:order-last ">
 
-            <img src="/img/about.png" class=" rounded-full">
+            <img src="/img/about.png" class="absolute rounded-full">
         </div>
+
     </div>
 
 
