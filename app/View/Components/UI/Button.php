884ed9397fa949fb87c8bcac39d005935303683c
Hussein Najah <98782046+i1Zeus@users.noreply.github.com>
2022-05-09 17:46:35 +0300

    Update Button.php

--- app/View/Components/UI/Button.php
+++ app/View/Components/UI/Button.php
@@ -21,29 +21,29 @@ class Button extends Component
 
             switch ($this->color) {
                 case "primary":
-                    $this->default .= 'bg-primary-500 hover:bg-primary-900 focus:ring-primary-300';
+                    $this->default .= 'bg-primary-500 hover:bg-primary-800 focus:ring-primary-300 ';
                     break;
                 case "secondary":
-                    $this->default .= 'bg-secondary-700 hover:bg-secondary-800 focus:ring-secondary-300';
+                    $this->default .= 'bg-secondary-700 hover:bg-secondary-800 focus:ring-secondary-300 ';
                     break;
                 case "success":
-                    $this->default .= 'bg-success-700 hover:bg-success-800 focus:ring-success-300';
+                    $this->default .= 'bg-success-700 hover:bg-success-800 focus:ring-success-300 ';
                     break;
                 case "error":
-                    $this->default .= 'bg-red-700 hover:bg-red-800 focus:ring-red-300';
+                    $this->default .= 'bg-red-700 hover:bg-red-800 focus:ring-red-300 ';
                     break;
                 case "warning":
-                    $this->default .= 'bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-200';
+                    $this->default .= 'bg-yellow-400 hover:bg-yellow-500 focus:ring-yellow-200 ';
                     break;
                 case "light":
-                    $this->default .= 'text-white bg-gray-600 border-gray-700 hover:bg-gray-900 focus:ring-gray-300';
+                    $this->default .= 'text-white bg-gray-600 border-gray-700 hover:bg-gray-900 focus:ring-gray-300 ';
                     break;
                 case "light2":
-                    $this->default .= 'text-gray-200 bg-gray-400 border-gray-600 hover:bg-gray-700 focus:ring-gray-300';
+                    $this->default .= 'text-gray-200 bg-gray-400 border-gray-600 hover:bg-gray-700 focus:ring-gray-300 ';
                     break;
             }
         } else
-            $this->default .= 'bg-red-500 hover:bg-red-800 focus:ring-red-300';
+            $this->default .= 'bg-red-500 hover:bg-red-800 focus:ring-red-300 ';
     }
 
     /**
