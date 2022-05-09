d61f4f2c5c77bd5b19b620944c360326b149af29
Mohammed Jawad <jawad6686@gmail.com>
2022-05-09 15:25:41 +0300

    add

--- resources/views/livewire/pages/donors/main.blade.php
+++ resources/views/livewire/pages/donors/main.blade.php
@@ -2,3 +2,15 @@
     <div class="grid grid-cols-3 gap-6">
         @for ($i = 0; $i < 3; $i++) @livewire('components.donors.card') @endfor </div>
     </div>
+@guest
+    <div class="hidden p-4 bg-[#d7e0e0] rounded-lg " id="requests" role="tabpanel" aria-labelledby="requests-tab">
+        @livewire('company.request.requests' , ['state' => 1])
+    </div>
+@endguest
+
+@admin
+    
+@endadmin
+@user
+    يوزر
+@enduser
