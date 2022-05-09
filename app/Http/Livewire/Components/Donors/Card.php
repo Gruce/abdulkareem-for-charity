a2ec40accd28e4c999bfb1263bfd50283af75e2a
Mohammed Jawad <jawad6686@gmail.com>
2022-05-09 22:58:34 +0300

    add

--- app/Http/Livewire/Components/Donors/Card.php
+++ app/Http/Livewire/Components/Donors/Card.php
@@ -6,7 +6,7 @@ use Livewire\Component;
 
 class Card extends Component
 {
-    public $name, $type, $shares, $photo, $date;
+    public $name, $type, $shares, $photo, $date, $email, $phone_number;
     public function render()
     {
         return view('livewire.components.donors.card');
--- resources/views/livewire/pages/donors/main.blade.php
+++ resources/views/livewire/pages/donors/main.blade.php
@@ -1,28 +1,58 @@
 <div>
-    {{-- <div class="grid grid-cols-3 gap-6">
-        @for ($i = 0; $i < 5; $i++) @livewire('components.donors.card') @endfor </div>
-    </div> --}}
-    @guest
-    <div class="grid grid-cols-3 gap-6" >
+
+    <div class="grid grid-cols-4 gap-6">
+        @guest
+
         @forelse ($users as $item)
         @livewire('components.donors.card' , [
-            'name' => $item->name,
-            'type' => $item->type,
-            'shares' => $item->shares_sum_share,
-            'photo' => $item->profile_photo_path,
-            'date' => $item->created_at,
-            ])
+        'name' => $item->name,
+        'type' => $item->type,
+        'shares' => $item->shares_sum_share,
+        'photo' => $item->profile_photo_path,
+        'date' => $item->created_at,
+        ])
         @empty
-            هلو
+        هلو
         @endforelse
+
+
+        @endguest
+
         
+        @user
+
+        @forelse ($users as $item)
+        @livewire('components.donors.card' , [
+        'name' => $item->name,
+        'email' => $item->email,
+        'type' => $item->type,
+        'shares' => $item->shares_sum_share,
+        'photo' => $item->profile_photo_path,
+        'date' => $item->created_at,
+        ])
+        @empty
+        هلو
+        @endforelse
+
+        @enduser
+        
+        
+        @admin
+        
+        @forelse ($users as $item)
+        @livewire('components.donors.card' , [
+        'name' => $item->name,
+        'email' => $item->email,
+        'type' => $item->type,
+        'shares' => $item->shares_sum_share,
+        'photo' => $item->profile_photo_path,
+        'date' => $item->created_at,
+        'phone_number' => $item->phone_number,
+        ])
+        @empty
+        هلو
+        @endforelse
+
+        @endadmin
     </div>
-    @endguest
-
-    @admin
-
-    @endadmin
-    @user
-    يوزر
-    @enduser
 </div>
