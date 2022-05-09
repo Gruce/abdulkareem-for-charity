2153874cb25cbefb4fb1ffcf18a6b74419f6ffe4
Mohammed Jawad <jawad6686@gmail.com>
2022-05-09 21:14:55 +0300

    add

--- resources/views/livewire/components/donors/card.blade.php
+++ resources/views/livewire/components/donors/card.blade.php
@@ -1,7 +1,7 @@
 <div>
 
     <div>
-        <div class=" p-4 w-full rounded-t-xl bg-gray-50 hover:bg-gray-100 transition duration-300">
+        <div class=" p-4 w-full rounded-t-xl bg-gray-50 hover:bg-gray-200 transition duration-300">
             <div class="grid grid-cols-5 mb-5">
                 <div class="col-span-2 flex flex-col">
                     <img src="https://images.unsplash.com/photo-1513364776144-60967b0f800f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80"
@@ -15,27 +15,9 @@
                 </div>
             </div>
 
-            <ul class="flex justify-start space-x-20 text-center text-lg" x-data="{ isOpen: false }">
 
-                <button @click="isOpen = !isOpen"
-                    class="m-2 text-white bg-primary-400 hover:bg-primary-500 focus:ring-1 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center ">
-                    زيادة الاسهم
-                </button>
 
-                <form class="flex flex-row-reverse" x-show="isOpen">
-                    <div>
-                        <input type="number" id="shares"
-                            class=" bg-gray-50 border border-gray-300  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block h-12 ml-9 m-4 py-5"
-                            placeholder="ادخل عدد الاسهم التي تريد اضافتها">
-                    </div>
-
-                    <button type="submit"
-                        class="text-white bg-primary-400 hover:bg-primary-500 focus:ring-1 focus:ring-cyan-300 font-medium rounded-full text-sm px-4 py-4  m-4">اضافة
-                    </button>
-                </form>
-            </ul>
-
-            {{-- <ul class=" flex justify-start space-x-4 text-center text-lg" x-data="{ isOpen: false}">
+            <ul class=" flex justify-start space-x-4 text-center text-lg" x-data="{ isOpen: false}">
                 <button @click="isOpen = !isOpen"
                     class="mt-2 ml-10 text-white bg-primary-400 hover:bg-primary-500 focus:ring-1 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">
                     زيادة الاسهم
@@ -50,7 +32,7 @@
                         class="mt-2 w-full text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-2 focus:ring-cyan-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2">اضافة
                     </button>
                 </form>
-            </ul> --}}
+            </ul>
 
             <h5 class="text-lg tracking-tight pt-5 text-center">البريد الالكتروني: jawad6686@gmail.com
             </h5>
