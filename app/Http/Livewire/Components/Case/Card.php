3cdc3c55d85fa4ee7f3d9ad7b7452859ae44516a
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-10 20:49:42 +0300

    show cases

--- app/Http/Livewire/Components/Case/Card.php
+++ app/Http/Livewire/Components/Case/Card.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class Card extends Component
 {
+    public $title, $description, $image_path, $file_path, $target, $received_price;
+    
     public function render()
     {
         return view('livewire.components.case.card');
