75c6f988060e6e1aa74fc94784ba67718cf76b76
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-05-10 18:16:09 +0300

    add

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -15,24 +15,36 @@
         <h1 class="mt-24 font-bold text-5xl text-primary-500">صندوق الاستاذ عبدالكريم <br>
         الخيري
         </h1>
-        <p class="font-semibold text-lg mt-10">
-            مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحاسوب وتكنلوجيا المعلومات
+        <p class="font-semibold text-lg mt-10 leading-9 text-gray-600">
+            مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحاسوب وتـكـنلوجيا المعـلومات
+            <br>
+            الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق للطلبة
+            <br>
+            ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
+            <span>
+                <a href="{{ route('about') }}" class="font-bold text-primary-500">
+                للمزيد من التفاصيل اضغط هنا
+                </a>
+            </span>
+            
         </p>
-        <div>
-            <button>
-                
-            </button>
+        <div class="mt-20">
+            <p class="mb-5 text-primary-900 text-2xl font-semibold">
+                من اجل استمرار العطاء اضغط هنا للتبرع
+                <a  href="{{ route('donate') }}"
+                class="text-white bg-primary-300 hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-600 font-medium rounded-xl text-sm px-5 py-2.5 text-center mb-2">
+                تبرع من هنا
+            </a>
+            </p>
         </div>
     </div>
         <div class="row-span-2 relative h-[36rem]">
             
-                <div class="absolute bottom-0 right-0  ">
+                <div class="absolute bottom-10 right-10  ">
                     <div class="h-[30rem] w-[38rem] ">
-                        <img class="h-full" src="/img/ppp.jpg" alt="">
+                        <img class="h-full" src="/img/home.jpg" alt="">
                     </div>
-                </div>
-              
-                
+                </div>   
         </div>
         
         <div class="col-span-2 row-span-1 bg-teal-200">03</div>
