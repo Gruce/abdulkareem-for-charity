a7884fc1cc410395f087dd2ab8ad93ee6b3debb1
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-05-10 14:22:29 +0300

    add

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -11,13 +11,24 @@
     <livewire:pages.home.admin.main />
     @endadmin --}}
 
-    <div class="container mx-auto grid grid-cols-2 grid-rows-6"><div class="row-span-2 bg-red-200">02</div>
-        <div class="row-span-2 bg-yellow-200 relative h-[36rem]">
+    <div class="container mx-auto grid grid-cols-2 grid-rows-6"><div class="row-span-2">
+        <h1 class="mt-24 font-bold text-5xl text-primary-500">صندوق الاستاذ عبدالكريم <br>
+        الخيري
+        </h1>
+        <p class="font-semibold text-lg mt-10">
+            مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحاسوب وتكنلوجيا المعلومات
+        </p>
+        <div>
+            <button>
+                
+            </button>
+        </div>
+    </div>
+        <div class="row-span-2 relative h-[36rem]">
             
-                <p>Relative parent</p>
                 <div class="absolute bottom-0 right-0  ">
                     <div class="h-[30rem] w-[38rem] ">
-                        <img class="rounded-md h-full" src="/img/ppp.jpg" alt="">
+                        <img class="h-full" src="/img/ppp.jpg" alt="">
                     </div>
                 </div>
               
