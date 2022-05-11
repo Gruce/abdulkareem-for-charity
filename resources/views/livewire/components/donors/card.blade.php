4dcf4878249c435efd684de0d2ab30cd1fda63ac
Mohammed Jawad <jawad6686@gmail.com>
2022-05-12 00:35:26 +0300

    add

--- resources/views/livewire/components/donors/card.blade.php
+++ resources/views/livewire/components/donors/card.blade.php
@@ -1,8 +1,8 @@
 <div>
 
-    <div class="grid grid-cols-4 gap-6" >
+    <div x-data="{ isOpen: false}" class="grid grid-cols-4 gap-6">
         @forelse ($users as $item)
-        <div 
+        <div
             class="shadow-sm shadow-gray-300 hover:shadow-gray-400 hover:shadow-lg p-4 w-full rounded-xl bg-gray-50 hover:bg-gray-200 transition duration-200">
             <div class="flex justify-end px-4 pt-4">
                 <button id="dropdownButton" data-dropdown-toggle="dropdown"
@@ -22,16 +22,9 @@
                             <a class="cursor-pointer w-full p-4" wire:click="confirm({{$item->id}})">
                                 <i class="text-red-600 fa-solid fa-trash"></i> Delete
                             </a>
-                            <hr>
+                            
                         </li>
-                        <li>
-                            <button
-                                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                type="button" data-modal-toggle="authentication-modal">
-                                Toggle modal
-                            </button>
-
-                        </li>
+                        
 
                     </ul>
                 </div>
@@ -64,35 +57,8 @@
         @empty
         لا يوجد متبرعين
         @endforelse
-        {{-- Modal --}}
-        <div id="authentication-modal" tabindex="-1" aria-hidden="true"
-            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-            <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-                <!-- Modal content -->
-                <div  class="relative bg-white rounded-lg shadow ">
-                    <button type="button"
-                        class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
-                        data-modal-toggle="authentication-modal">
-                        <i class="fa-solid fa-xmark"></i>
-                    </button>
-                    <div class="py-6 px-6 lg:px-8">
-                        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">زيادة الاسهم</h3>
-                        <form wire:submit.prevent="add({{ $item->id }} )" class="space-y-6" action="#">
-                            <div>
-                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">عدد
-                                    الاسهم المضافة</label>
-                                <input wire:moder="share" type="number">
-                            </div>
 
-
-                            <button type="submit"
-                                class="w-full text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">اضافة
-                                الاسهم</button>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
+
+
 </div>
