db39a45bb43e4a6243df200c4b5f6a1e7c51dc09
Mohammed Jawad <jawad6686@gmail.com>
2022-05-12 19:46:49 +0300

    add

--- resources/views/livewire/components/home/information.blade.php
+++ resources/views/livewire/components/home/information.blade.php
@@ -1,17 +1,17 @@
 <div>
-    <div class="grid grid-cols-3">
+    <div class="grid grid-cols-3 p-10 ">
         @forelse ($menu->items as $item)
-            <div class="flex ">
+            <div class="flex justify-center">
                 <div class="">
-                    <i class="{{ $item->icon }} text-red-400 text-7xl"></i>
+                    <i class="{{ $item->icon }} text-white text-7xl ml-4"></i>
                 </div>
 
-                <div class="flex flex-col">
-                    <span>
+                <div class="flex flex-col p-2 ">
+                    <span class="text-white text-3xl">
                         {{ $item->value }}
                     </span>
 
-                    <span>
+                    <span class="text-white text-lg">
                         {{ $item->info }}
                     </span>
                 </div>
--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -11,44 +11,45 @@
     <livewire:pages.home.admin.main />
     @endadmin --}}
 
-    <div class="container mx-auto grid grid-cols-2 grid-rows-6"><div class="row-span-2 bg-red-200">
-        <h1 class="mt-24 font-bold text-5xl text-primary-500">صندوق الاستاذ عبدالكريم <br>
-        الخيري
-        </h1>
-        <p class="font-semibold text-lg mt-10 leading-9 text-gray-600">
-            مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحـاسوب وتـكنلوجيا المعـلومات
-            <br>
-            الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق للطلبة
-            <br>
-            ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
-            <span>
-                <a href="{{ route('about') }}" class="font-bold text-primary-500">
-                للمزيد من التفاصيل اضغط هنا
-                </a>
-            </span>
-            
-        </p>
-        <div class="mt-20">
-            <p class="mb-5 text-primary-500 text-2xl font-semibold">
-                من اجل أستمرار العطاء اضغط هنا للتبرع
-                <a  href="{{ route('donate') }}"
-                class="text-white bg-primary-500 hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-600 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
-                تبرع من هنا
-            </a>
+    <div class="container mx-auto grid grid-cols-2 grid-rows-8">
+        <div class="col-span-1 row-span-3 bg-stone-300 p-16">
+            <h1 class=" font-bold text-4xl text-primary-500">صندوق الاستاذ عبدالكريم الخيري</h1>
+            <p class="font-semibold text-lg mt-10 leading-9 text-gray-600">
+                مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحـاسوب وتـكنلوجيا المعـلومات
+                <br>
+                الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق للطلبة
+                <br>
+                ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
+                <span>
+                    <a href="{{ route('about') }}" class="font-bold text-primary-500">
+                        للمزيد من التفاصيل اضغط هنا
+                    </a>
+                </span>
+
             </p>
+            <div class="mt-20">
+                <p class="mb-5 text-primary-500 text-2xl font-semibold">
+                    من اجل أستمرار العطاء اضغط هنا للتبرع
+                    <a href="{{ route('donate') }}"
+                        class="text-white bg-primary-500 hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-600 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
+                        تبرع من هنا
+                    </a>
+                </p>
+            </div>
         </div>
-    </div>
-        <div class="row-span-2 bg-green-200 relative h-[36rem]">
-            
-                <div class="absolute bottom-10 right-10  ">
-                    <div class="h-[30rem] w-[38rem] ">
-                        <img class="h-full" src="/img/home.jpg" alt="">
-                    </div>
-                </div>   
+
+        <div class="col-span-1 row-span-3 bg-secondary-300 relative h-[36rem]">
+
+            <div class="absolute bottom-0 right-0  ">
+                <div class="h-[30rem] w-[40rem] ">
+                    <img class="h-full" src="/img/home.jpg" alt="">
+                </div>
+            </div>
         </div>
         
-        <div class="col-span-2 row-span-1 bg-teal-200">@livewire('components.home.information')</div>
-        <div class="col-span-2 row-span-3 bg-purple-200">04</div>
+
+        <div class="col-span-2 row-span-1 bg-primary-800 ">@livewire('components.home.information')</div>
+        <div class="col-span-2 row-span-4 bg-purple-200 text-9xl">04</div>
     </div>
 
 
