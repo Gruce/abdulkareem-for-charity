9e9dccb717f71e5c4124b1b16ff9c530f563a9bf
Mohammed Jawad <jawad6686@gmail.com>
2022-05-12 20:33:22 +0300

    add

--- resources/views/livewire/components/home/information.blade.php
+++ resources/views/livewire/components/home/information.blade.php
@@ -1,21 +1,22 @@
 <div>
     <div class="grid grid-cols-3 p-10 ">
         @forelse ($menu->items as $item)
-            <div class="flex justify-center">
-                <div class="">
-                    <i class="{{ $item->icon }} text-white text-7xl ml-4"></i>
-                </div>
+        <div class="flex justify-center">
+            <div class="flex flex-col p-2 ">
+                <span class="text-white text-3xl">
+                    {{ $item->value }}
+                </span>
 
-                <div class="flex flex-col p-2 ">
-                    <span class="text-white text-3xl">
-                        {{ $item->value }}
-                    </span>
-
-                    <span class="text-white text-lg">
-                        {{ $item->info }}
-                    </span>
-                </div>
+                <span class="text-white text-lg">
+                    {{ $item->info }}
+                </span>
             </div>
+            <div class="">
+                <i class="{{ $item->icon }} text-white text-7xl ml-4"></i>
+            </div>
+
+
+        </div>
         @empty
         @endforelse
     </div>
--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -13,7 +13,7 @@
 
     <div class="container mx-auto grid grid-cols-2 grid-rows-8">
         <div class="col-span-1 row-span-3 bg-stone-300 p-16">
-            <h1 class=" font-bold text-4xl text-primary-500">صندوق الاستاذ عبدالكريم الخيري</h1>
+            <h1 class=" font-bold font-Kufi text-7xl text-primary-500">صندوق الاستاذ عبدالكريم الخيري</h1>
             <p class="font-semibold text-lg mt-10 leading-9 text-gray-600">
                 مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحـاسوب وتـكنلوجيا المعـلومات
                 <br>
@@ -29,24 +29,34 @@
             </p>
             <div class="mt-20">
                 <p class="mb-5 text-primary-500 text-2xl font-semibold">
-                    من اجل أستمرار العطاء اضغط هنا للتبرع
+
                     <a href="{{ route('donate') }}"
                         class="text-white bg-primary-500 hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-primary-600 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2">
-                        تبرع من هنا
+                        تبرع الان
                     </a>
                 </p>
             </div>
         </div>
 
-        <div class="col-span-1 row-span-3 bg-secondary-300 relative h-[36rem]">
-
-            <div class="absolute bottom-0 right-0  ">
-                <div class="h-[30rem] w-[40rem] ">
-                    <img class="h-full" src="/img/home.jpg" alt="">
+        <div class="col-span-1 row-span-3 bg-primary-200 relative h-[36rem]">
+            <ul class="flex gap-4 p-10 ">
+                <li>
+                    <a href="/">عن الصندوق </a>
+                </li>
+                <li>
+                    <a href="/">المتبرعين </a>
+                </li>
+                <li>
+                    <a href="/"> الحالات</a>
+                </li>
+            </ul>
+            <div class="absolute bottom-0 right-0 ">
+                <div class="h-[30rem] w-[36rem] ">
+                    <img class="h-full rounded-l-full" src="/img/home.jpg" alt="">
                 </div>
             </div>
         </div>
-        
+
 
         <div class="col-span-2 row-span-1 bg-primary-800 ">@livewire('components.home.information')</div>
         <div class="col-span-2 row-span-4 bg-purple-200 text-9xl">04</div>
