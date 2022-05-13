95e5509f917395d4fa11e7df286666862e94f153
Mohammed Jawad <jawad6686@gmail.com>
2022-05-13 16:38:56 +0300

    add

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -2,8 +2,8 @@
     <div class="container mx-auto grid grid-cols-2 grid-rows-8">
 
         {{-- Introduction --}}
-        <div class="col-span-1 row-span-3 bg-tertiary-100 p-16">
-            <h1 class="font-Kufi text-7xl text-[#D94C57]">صندوق الاستاذ عبدالكريم الخيري</h1>
+        <div class="col-span-1 row-span-3 bg-[#a2bf39] p-16">
+            <h1 class="font-Kufi text-7xl text-[#f2f2f2]">صندوق الاستاذ عبدالكريم الخيري</h1>
             <p class="font-bold font-IBM text-lg mt-10 leading-9 text-gray-800">
                 مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحـاسوب وتـكنلوجيا المعـلومات
                 <br>
@@ -18,7 +18,7 @@
 
                 <div>
                     <a href="{{ route('donate') }}"
-                        class="text-white text-base bg-secondary-500 hover:bg-secondary-800 focus:outline-none font-medium hover:scale-110 duration-200 rounded-lg  px-5 py-2.5 text-center mb-2">
+                        class="text-gray-900 text-base bg-[#f2f2f2] hover:bg-secondary-800 focus:outline-none font-medium hover:scale-110 duration-200 rounded-lg  px-5 py-2.5 text-center mb-2">
                         تبرع الان
                     </a>
                 </div>
@@ -27,8 +27,8 @@
                 <div class="flex flex-col ">
                     <span class="text-center">
                         <a href="{{ route('about') }}"
-                            class=" bg-secondary-500 duration-200 hover:bg-secondary-800 rounded-full px-5 py-2  text-center mb-2">
-                            <i class="fa-solid fa-info text-2xl text-white"></i>
+                            class="  bg-[#f2f2f2] duration-200 hover:bg-secondary-800 rounded-full px-5 py-2  text-center mb-2">
+                            <i class="fa-solid fa-info text-2xl text-gray-900"></i>
                         </a>
                     </span>
                     <span class="mt-4 text-lg font-mono">
@@ -40,7 +40,7 @@
         </div>
 
         {{-- photo --}}
-        <div class="col-span-1 row-span-3 bg-primary-300 relative h-[36rem]">
+        <div class="col-span-1 row-span-3 bg-[#f2f2f2] relative h-[36rem]">
             <ul class="flex gap-12 p-10 justify-center text-gray-900">
                 <li>
                     <a href="/">عن الصندوق </a>
@@ -66,7 +66,7 @@
         </div>
 
         {{-- information --}}
-        <div class="col-span-2 row-span-1 bg-primary-300 ">@livewire('components.home.information')</div>
+        <div class="col-span-2 row-span-1 bg-[#f2f2f2] ">@livewire('components.home.information')</div>
         
         {{-- Contect --}}
         <div class="col-span-2 row-span-4 bg-tertiary-100 text-9xl">04</div>
