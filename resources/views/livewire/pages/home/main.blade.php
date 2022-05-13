6fae7cf207e64b6e7785b39e08e99549e02fd8fc
Mohammed Jawad <jawad6686@gmail.com>
2022-05-13 13:28:37 +0300

    add

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="container mx-auto grid grid-cols-2 grid-rows-8">
         <div class="col-span-1 row-span-3 bg-[#EDF7DA] p-16">
-            <h1 class="font-Kufi text-7xl text-primary-500">صندوق الاستاذ عبدالكريم الخيري</h1>
+            <h1 class="font-Kufi text-7xl text-[#D94C57]">صندوق الاستاذ عبدالكريم الخيري</h1>
             <p class="font-bold font-IBM text-lg mt-10 leading-9 text-gray-800">
                 مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحـاسوب وتـكنلوجيا المعـلومات
                 <br>
@@ -10,31 +10,46 @@
                 ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
 
 
+
             </p>
             <div class="flex flex-row gap-80 mt-20">
-                <p class="mb-5 text-primary-500 text-2xl font-semibold">
 
+                <div>
                     <a href="{{ route('donate') }}"
-                        class="text-white bg-primary-300 hover:bg-primary-600 focus:outline-none font-medium hover:scale-110 duration-200 rounded-lg text-sm px-5 py-2.5 text-center mb-2">
+                        class="text-white text-lg bg-secondary-500 hover:bg-primary-600 focus:outline-none font-medium hover:scale-110 duration-200 rounded-lg  px-5 py-2.5 text-center mb-2">
                         تبرع الان
                     </a>
-                </p>
-                <span>
-                    <a href="{{ route('about') }}"
-                        class=" bg-primary-300 duration-200 hover:bg-primary-600 rounded-full px-5 py-2">
-                        <i class="fa-solid fa-info text-2xl"></i>
-                    </a>
-                </span>
+                </div>
+
+
+                <div class="flex flex-col ">
+                    <span class="text-center">
+                        <a href="{{ route('about') }}"
+                            class=" bg-[#D94C57] duration-200 hover:bg-primary-600 rounded-full px-5 py-2  text-center mb-2">
+                            <i class="fa-solid fa-info text-2xl text-white"></i>
+                        </a>
+                    </span>
+                    <span class="mt-4 text-lg font-mono">
+                        لمعرفة المزيد
+                    </span>
+                </div>
+
             </div>
         </div>
         {{-- --}} {{-- --}} {{-- --}} {{-- --}}
         <div class="col-span-1 row-span-3 bg-[#A1D6B5] relative h-[36rem]">
-            <ul class="flex gap-12 p-10 justify-center">
+            <ul class="flex gap-12 p-10 justify-center text-[#34734c]">
                 <li>
                     <a href="/">عن الصندوق </a>
                 </li>
                 <li>
+                    <a href="/">.</a>
+                </li>
+                <li>
                     <a href="/">المتبرعين </a>
+                </li>
+                <li>
+                    <a href="/">.</a>
                 </li>
                 <li>
                     <a href="/"> الحالات</a>
