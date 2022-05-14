a14eefec541274a7a59545f78131cbd60c4f160d
Mohammed Jawad <jawad6686@gmail.com>
2022-05-14 22:07:34 +0300

    add

--- resources/views/livewire/components/home/information.blade.php
+++ resources/views/livewire/components/home/information.blade.php
@@ -1,24 +1,43 @@
-<div>
-    <div class="grid grid-cols-3 p-10 ">
-        @forelse ($menu->items as $item)
-        <div class="flex justify-center">
-            <div class="flex flex-col p-2 text-center">
-                <span class="text-white text-3xl">
-                    {{ $item->value }}
-                </span>
+@if($state)
 
-                <span class="text-white text-lg">
-                    {{ $item->info }}
-                </span>
-            </div>
-            <div class="">
-                <i class="{{ $item->icon }} text-white text-7xl ml-4"></i>
-            </div>
+{{-- activites --}}
+@forelse ($menu->items as $item)
 
-
-        </div>
-        @empty
-        
-        @endforelse
+<div class="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md ">
+    <div class="flex flex-col p-2 text-center mx-20">
+        <i class="fa-solid fa-{{ $item->value }}"></i>
+    
+        <span class="text-xl">
+            {{ $item->info }}
+        </span>
     </div>
+    
+    
 </div>
+
+
+@empty
+
+@endforelse
+
+
+@else
+{{-- info --}}
+@forelse ($menu->items as $item)
+
+<div class="flex flex-col p-2 text-center mx-20">
+    <span class="text-5xl font-bold font-noto">
+        {{ $item->value }}
+    </span>
+
+    <span class="text-xl">
+        {{ $item->info }}
+    </span>
+</div>
+
+
+@empty
+
+@endforelse
+
+@endif
--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -17,32 +17,31 @@
             <div class="flex flex-row gap-80 mt-20">
 
                 <div>
-                    <a href="{{ route('donate') }}"
-class="text-black text-base bg-[#f2f2f2] hover:bg-primary-600 hover:text-white focus:outline-none font-medium
+                    <a href="{{ route('donate') }}" class="text-black text-base bg-[#f2f2f2] hover:bg-primary-600 hover:text-white focus:outline-none font-medium
 hover:scale-110 duration-200 rounded-lg px-5 py-2.5 text-center mb-2">
-تبرع الان
-</a>
-</div>
+                        تبرع الان
+                    </a>
+                </div>
 
 
-<div class="flex flex-col">
-    <span class="text-center">
-        <a href="{{ route('about') }}"
-            class="  bg-[#f2f2f2] duration-200 hover:bg-primary-600 rounded-full px-6 py-2 text-center mb-2">
-            <i class="fa-solid fa-info text-lg text-black"></i>
-        </a>
-    </span>
-    <span class="mt-4 text-lg font-mono text-[#F2f2f2]">
-        لمعرفة المزيد
-    </span>
-</div>
+                <div class="flex flex-col">
+                    <span class="text-center">
+                        <a href="{{ route('about') }}"
+                            class="  bg-[#f2f2f2] duration-200 hover:bg-primary-600 rounded-full px-6 py-2 text-center mb-2">
+                            <i class="fa-solid fa-info text-lg text-black"></i>
+                        </a>
+                    </span>
+                    <span class="mt-4 text-lg font-mono text-[#F2f2f2]">
+                        لمعرفة المزيد
+                    </span>
+                </div>
 
-</div>
-</div>
+            </div>
+        </div>
 
-{{-- photo
+        {{-- photo
         <div class="col-span-1 row-span-3 bg-[#548C62] relative h-[36rem]">
-            
+
             <div class="absolute bottom-0 right-0 ">
                 <div class="h-[34rem] w-[40rem] ">
                     <img class="h-full rounded-l-full" src="/img/home.jpg" alt="">
@@ -50,9 +49,9 @@ hover:scale-110 duration-200 rounded-lg px-5 py-2.5 text-center mb-2">
             </div>
         </div>
 
-        {{-- information 
+        {{-- information
         <div class="col-span-2 row-span-1 bg-[#548C62] ">@livewire('components.home.information')</div>
-        
+
         {{-- Contect -
         <div class="col-span-2 row-span-4 bg-white text-9xl">04</div>
     </div>
@@ -62,10 +61,10 @@ hover:scale-110 duration-200 rounded-lg px-5 py-2.5 text-center mb-2">
     <div class="flex flex-col h-screen">
 
         <!-- INTRO -->
-        <div class="flex basis-4/12 bg-red-50 p-16">
+        <div class="grid grid-cols-12 basis-4/12   justify-between ">
             <!-- Text -->
-            <div class="basis-6/12 p-6">
-                <h1 class="text-secondary-900 text-8xl font-bold ">
+            <div class="col-span-5 pr-20 pt-16  ">
+                <h1 class="text-secondary-900 text-7xl font-bold ">
                     تبرع بسهولة
                 </h1>
                 <p class="text-secondary-200 text-3xl mt-6">
@@ -76,14 +75,40 @@ hover:scale-110 duration-200 rounded-lg px-5 py-2.5 text-center mb-2">
                     الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق
                     <br>
                     للطلبة ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
+
+                </p>
             </div>
-            <div class="basis-6/12">
-                <img src="/img/charity.png" class="h-60">
+
+
+            {{-- Photo --}}
+
+            <div class="col-span-7    ">
+                <img src="/img/charity.png" class="w-10/12 h-auto m-auto">
             </div>
         </div>
 
-        <div class="flex basis-2/12 bg-blue-600">2</div>
-        <div class="flex basis-4/12 bg-red-600">3</div>
-        <div class="flex basis-2/12 bg-blue-600">4</div>
+        {{-- information --}}
+        <div class="flex basis-2/12 justify-center mt-14">@livewire('components.home.information')</div>
+
+        {{-- Cases --}}
+        <div class="flex basis-4/12 pr-20 pt-16">
+            <span class="text-secondary-900 text-6xl font-bold ">
+                اخر الحالات
+            </span>
+            <div class="my-20 flex gap-6">
+                @livewire('components.case.card')
+            </div>
+
+        </div>
+
+        {{-- Work for --}}
+        <div class="flex basis-2/12  pr-20 pt-16">
+            <span class="text-secondary-900 text-6xl font-bold ">
+                نحن نعمل لأجل
+            </span>
+            <div class="my-20 flex gap-6 justify-center">
+                @livewire('components.home.information',['state' => 1])
+            </div>
+        </div>
     </div>
 </div>
