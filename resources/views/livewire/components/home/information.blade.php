87b66faff8434db007cd126e15f57981dc6b7af9
Mohammed Jawad <jawad6686@gmail.com>
2022-05-14 23:00:42 +0300

    Update information.blade.php

--- resources/views/livewire/components/home/information.blade.php
+++ resources/views/livewire/components/home/information.blade.php
@@ -6,13 +6,13 @@
 <div class="p-4 bg-white rounded-lg border border-gray-200 shadow-md m-5">
     <div class="flex flex-col p-2 text-center mx-10">
         <i class="fa-solid fa-{{ $item->value }} text-7xl text-primary-500"></i>
-    
+
         <span class="text-2xl font-bold mt-2">
             {{ $item->info }}
         </span>
     </div>
-    
-    
+
+
 </div>
 
 
