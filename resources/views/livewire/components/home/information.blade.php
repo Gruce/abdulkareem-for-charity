4ab3e8cc6925a1d26cf630c2e77ddd1ba1ae64da
Mohammed Jawad <jawad6686@gmail.com>
2022-05-14 17:43:36 +0300

    add

--- resources/views/livewire/components/home/information.blade.php
+++ resources/views/livewire/components/home/information.blade.php
@@ -3,17 +3,15 @@
         @forelse ($menu->items as $item)
         <div class="flex justify-center">
             <div class="flex flex-col p-2 text-center">
-                <span class="text-white text-3xl">
+                <span class=" text-3xl">
                     {{ $item->value }}
                 </span>
 
-                <span class="text-white text-lg">
+                <span class=" text-lg">
                     {{ $item->info }}
                 </span>
             </div>
-            <div class="">
-                <i class="{{ $item->icon }} text-white text-7xl ml-4"></i>
-            </div>
+            
 
 
         </div>
