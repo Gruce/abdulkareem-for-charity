aeb975e9e8247450d4d3affe864fa952f2ce36bd
Ahmed <mr.darkboss1@gmail.com>
2022-05-14 23:33:54 +0300

    button

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -4,11 +4,11 @@
         <!-- INTRO -->
         <div class="grid grid-cols-12 basis-4/12   justify-between ">
             <!-- Text -->
-            <div class="col-span-5 pr-20 pt-16  ">
-                <h1 class="text-secondary-900 text-7xl font-bold ">
+            <div class="col-span-5 pr-24 pt-20  ">
+                <h1 class="text-secondary-900 text-6xl font-bold ">
                     تبرع بسهولة
                 </h1>
-                <p class="text-secondary-200 text-3xl mt-6">
+                <p class="text-secondary-200 text-2xl mt-6">
                     مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم
                     <br>
                     الحـاسوب وتـكنلوجيا المعـلومات الهدف منه رعاية الاعمال
@@ -18,12 +18,13 @@
                     للطلبة ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
 
                 </p>
+                <div class="mt-10 mr-4">
+                <button type="button" class="text-white text-2xl bg-primary-500 hover:bg-primary-900 focus:ring-4 focus:ring-primary-300  rounded-lg  px-5 py-2.5 mr-2 mb-2 ">تبرع الان</button>
+                </div>
             </div>
-
-
             {{-- Photo --}}
 
-            <div class="col-span-7    ">
+            <div class="col-span-7">
                 <img src="/img/charity.png" class="w-10/12 h-auto m-auto">
             </div>
         </div>
