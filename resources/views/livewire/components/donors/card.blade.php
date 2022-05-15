b41bdc9b8b1897f30c99c722a204c66d780f05c0
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-16 01:02:04 +0300

    Update card.blade.php

--- resources/views/livewire/components/donors/card.blade.php
+++ resources/views/livewire/components/donors/card.blade.php
@@ -76,9 +76,9 @@
                                         <div>
                                             <button type="button"
                                                 wire:click="accept({{ $share->id }}, {{ $share->state }})"
-                                                class="focus:outline-none text-white bg-green-500 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">قبول</button>
+                                                class="focus:outline-none text-white bg-green-500 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2">قبول</button>
                                             <button type="button" wire:click="deleteShare({{ $share->id }})"
-                                                class="focus:outline-none text-white bg-red-500 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">حذف</button>
+                                                class="focus:outline-none text-white bg-red-500 hover:bg-red-500 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2 mr-2 mb-2">حذف</button>
                                         </div>
                                     </div>
                                 @endforeach
