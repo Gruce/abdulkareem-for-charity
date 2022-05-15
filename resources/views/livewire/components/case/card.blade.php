8a18fe41cd5a0a1a0c0f2e61edb63c0cdd8b7cd9
Mohammed Jawad <jawad6686@gmail.com>
2022-05-15 16:04:13 +0300

    add

--- resources/views/livewire/components/case/card.blade.php
+++ resources/views/livewire/components/case/card.blade.php
@@ -1,90 +1,105 @@
 @forelse ($events as $item)
-    <div
-        class="rounded-lg  border-2 border-primary-100 hover:shadow-lg hover:border-primary-500 w-96 h-auto group">
-        @admin
-        {{-- start dropdown --}}
-        <div class="flex justify-end relative">
+<div class="rounded-lg  border-2 border-primary-100 hover:shadow-lg hover:border-primary-500 w-96 h-auto group">
+    {{--
+    @admin
+    start dropdown --}}
+    {{-- <div class="flex justify-end relative">
 
-            <button id="dropdownButton-{{ $item->id }}" data-dropdown-toggle="dropdown-{{ $item->id }}"
-                class="absolute text-gray-800 focus:outline-none hover:bg-gray-300 text-xl rounded-md px-2.5 py-2.5"
-                type="button">
-                <i class="fa-solid fa-ellipsis-vertical "></i>
-            </button>
-            <!-- Dropdown menu -->
-            <div id="dropdown-{{ $item->id }}"
-                class="hidden z-10 rounded divide-y divide-gray-100 shadow bg-gray-50">
-                <ul class="text-base" aria-labelledby="dropdownButton-{{ $item->id }}">
-                    <div class="group p-2">
-                        <a href={{ route('edit-case', ['case_id' => $item->id]) }} class=" w-full hover:bg-gray-200">
-                            <i
-                                class="text-green-400 fa-solid fa-pen-to-square text-xl hover:scale-110 hover:text-green-600 hover:rotate-12"></i>
-                            تعديل
-                        </a>
-                        <button wire:click="confirm({{ $item->id }})" class=" w-full hover:bg-gray-200">
-                            <i class="text-red-400 fa-solid fa-trash text-xl hover:scale-110 hover:text-red-600"></i>
-                            حذف
-                        </button>
-                    </div>
-
-                </ul>
-            </div>
-        </div>
-        @endadmin
-        {{-- end dropdown --}}
-
-        {{-- case Image --}}
-        <div class="rounded-lg">
-            {{-- <img class="rounded-t-lg" src="{{ asset($item->image_path) }}" alt="" /> --}}
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI7Vm3bWFcLUw9o1XiIVwWv3Vcuitvy9GOCg&usqp=CAU"
-                class="pl-3 pt-3 pr-3 w-full rounded-lg" >
-        </div>
-
-        {{-- title + more info --}}
-        <div class="p-6">
-            <h5 class="text-center font-bold group-hover:text-green-600 text-3xl mb-2"> {{ $item->title }}</h5>
-            <div class=" xs:28">
-                <p class="text-gray-900 text-medium mb-4">
-                    {{ Str::limit($item->description, 140) }}
-
-                    {{-- more info button --}}
-                    <button class="text-primary-500 hover:scale-110 font-bold duration-300 mb-4" type="button"
-                        data-modal-toggle="defaultModal">
-                        قراءة المزيد
+        <button id="dropdownButton-{{ $item->id }}" data-dropdown-toggle="dropdown-{{ $item->id }}"
+            class="absolute text-gray-800 focus:outline-none hover:bg-gray-300 text-xl rounded-md px-2.5 py-2.5"
+            type="button">
+            <i class="fa-solid fa-ellipsis-vertical "></i>
+        </button>
+        <!-- Dropdown menu -->
+        <div id="dropdown-{{ $item->id }}" class="hidden z-10 rounded divide-y divide-gray-100 shadow bg-gray-50">
+            <ul class="text-base" aria-labelledby="dropdownButton-{{ $item->id }}">
+                <div class="group p-2">
+                    <a href={{ route('edit-case', ['case_id'=> $item->id]) }} class=" w-full hover:bg-gray-200">
+                        <i
+                            class="text-green-400 fa-solid fa-pen-to-square text-xl hover:scale-110 hover:text-green-600 hover:rotate-12"></i>
+                        تعديل
+                    </a>
+                    <button wire:click="confirm({{ $item->id }})" class=" w-full hover:bg-gray-200">
+                        <i class="text-red-400 fa-solid fa-trash text-xl hover:scale-110 hover:text-red-600"></i>
+                        حذف
                     </button>
-                </p>
-                <div id="defaultModal" tabindex="-1" aria-hidden="true"
-                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
-                    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                        <div class="relative bg-white rounded-lg shadow ">
-                            <button type="button"
-                                class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-red-600 rounded-bl-lg rounded-tr-lg p-3 ml-auto inline-flex items-center "
-                                data-modal-toggle="defaultModal">
-                                <i class="fa-solid fa-xmark text-2xl"></i>
-                            </button>
-                            <div class="flex justify-center items-center rounded-t border-b ">
-                                <h5 class="text-center font-bold text-primary-900 text-3xl">
-                                    {{ $item->title }}
-                                </h5>
-                            </div>
+                </div>
 
-                            <div class="p-6 space-y-6">
-                                <p class="text-base font-semibold leading-relaxed text-gray-600">
-                                    {{ $item->description }}
-                                </p>
-                            </div>
+            </ul>
+        </div>
+    </div>
+    @endadmin --}}
+    {{-- end dropdown --}}
+
+    {{-- case Image --}}
+    <div class="pt-3 px-3">
+        {{-- <img class="rounded-t-lg" src="{{ asset($item->image_path) }}" alt="" /> --}}
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI7Vm3bWFcLUw9o1XiIVwWv3Vcuitvy9GOCg&usqp=CAU"
+            class=" w-full rounded-lg ">
+    </div>
+
+    {{-- title + more info --}}
+    <div class="p-6">
+        <h5 class="text-center font-bold group-hover:text-green-600 text-3xl mb-2"> {{ $item->title }}</h5>
+        <div class=" xs:28">
+            <p class="text-gray-900 text-medium mb-4">
+                {{ Str::limit($item->description, 140) }}
+
+                {{-- more info button --}}
+                <button class="text-primary-500 hover:scale-110 font-bold duration-300 mb-4" type="button"
+                    data-modal-toggle="defaultModal">
+                    قراءة المزيد
+                </button>
+            </p>
+            <div id="defaultModal" tabindex="-1" aria-hidden="true"
+                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
+                <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                    <div class="relative bg-white rounded-lg shadow ">
+                        <button type="button"
+                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-red-600 rounded-bl-lg rounded-tr-lg p-3 ml-auto inline-flex items-center "
+                            data-modal-toggle="defaultModal">
+                            <i class="fa-solid fa-xmark text-2xl"></i>
+                        </button>
+                        <div class="flex justify-center items-center rounded-t border-b ">
+                            <h5 class="text-center font-bold text-primary-900 text-3xl">
+                                {{ $item->title }}
+                            </h5>
+                        </div>
+
+                        <div class="p-6 space-y-6">
+                            <p class="text-base font-semibold leading-relaxed text-gray-600">
+                                {{ $item->description }}
+                            </p>
                         </div>
                     </div>
                 </div>
-                <hr class="mb-5">
-
-                <div class="font-bold font-noto text-xl group-hover:text-green-600 text-center mb-2">
+            </div>
+            <hr class="mb-5">
+            <div class="flex justify-center @admin justify-between @endadmin">
+                <div class="font-bold font-noto text-xl group-hover:text-green-600 text-center ">
                     الهدف : {{ $item->target }} د.ع
+
+
+                </div>
+                <div class="text-right mt-2">@admin
+                    <button wire:click="confirm({{ $item->id }})" class="mx-2">
+                        <i class="text-red-400 fa-solid fa-trash text-xl hover:text-red-600"></i>
+
+                    </button>
+                    <a href={{ route('edit-case', ['case_id'=> $item->id]) }} class="mx-2">
+                        <i class="text-primary-400 fa-solid fa-pen-to-square text-xl hover:text-primary-600"></i>
+
+                    </a>
+
+                    @endadmin
                 </div>
             </div>
+
         </div>
     </div>
+</div>
 @empty
-    <div class="text-2xl font-medium text-gray-700">
-        لايوجد حالات
-    </div>
+<div class="text-2xl font-medium text-gray-700">
+    لايوجد حالات
+</div>
 @endforelse
--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -18,11 +18,11 @@
                     للطلبة ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
                 </p>
                 <div class="mt-10 mr-4 flex gap-5">
-                    <button type="button"
+                    <a href="{{ route('donate') }}"
                         class="text-white text-2xl bg-primary-500 hover:bg-white  border border-transparent hover:border-green-500 hover:text-primary-500 focus:ring-4 focus:ring-primary-300  rounded-lg  px-5 py-2.5 mr-2 mb-2 ">تبرع
-                        الان</button>
-                    <button type="button"
-                        class="hover:text-white text-2xl hover:bg-primary-500 bg-white  border hover:border-transparent border-green-500 text-primary-500 focus:ring-4 focus:ring-primary-300  rounded-lg  px-5 py-2.5 mr-2 mb-2 ">الحالات</button>
+                        الان</a>
+                    <a href="{{ route('cases') }}"
+                        class="hover:text-white text-2xl hover:bg-primary-500 bg-white  border hover:border-transparent border-green-500 text-primary-500 focus:ring-4 focus:ring-primary-300  rounded-lg  px-5 py-2.5 mr-2 mb-2 ">الحالات</a>
                 </div>
             </div>
             {{--Photo--}}
@@ -36,7 +36,7 @@
         <div class="flex basis-2/12 justify-center mt-14">@livewire('components.home.information')</div>
 
         {{--Cases--}}
-        <div class="flex basis-4/12 pr-20 pt-16">
+        <div class="flex basis-4/12 pr-24 pt-16">
             <span class="text-secondary-900 text-6xl font-bold ">
                 اخر الحالات
             </span>
@@ -52,8 +52,9 @@
                 نحن نعمل لأجل
             </span>
             <div class="my-20 flex gap-6 justify-center">
-                <blade
-                    livewire|(%26%2339%3Bcomponents.home.information%26%2339%3B%2C%5B%26%2339%3Bstate%26%2339%3B%20%3D%3E%201%5D)%0D>
+                {{-- <blade
+                    livewire|(%26%2339%3Bcomponents.home.information%26%2339%3B%2C%5B%26%2339%3Bstate%26%2339%3B%20%3D%3E%201%5D)%0D> --}}
+                    @livewire('components.home.information',['state' => true])
             </div>
         </div>
     </div>
