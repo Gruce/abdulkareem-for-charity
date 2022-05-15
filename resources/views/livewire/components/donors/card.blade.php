fa10bab24d474861a9089b1646085da8931771e8
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-16 02:56:01 +0300

    Update card.blade.php

--- resources/views/livewire/components/donors/card.blade.php
+++ resources/views/livewire/components/donors/card.blade.php
@@ -69,7 +69,7 @@
                                     <div
                                         class="px-5 py-2 text-3xl font-bold text-gray-600 bg-gray-200 rounded-lg flex justify-between">
                                         <div>
-                                            <h3 class="text-xl mr-2 ">تبرع :
+                                            <h3 class="text-xl mr-2">تبرع :
                                                 <span>{{ $share->share }}</span>
                                             </h3>
                                         </div>
