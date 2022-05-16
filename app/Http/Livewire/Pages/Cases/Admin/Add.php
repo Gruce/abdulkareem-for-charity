e9a76e7521dd8a823b4b26da3f6f9eeb5a2f0619
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-05-16 21:32:25 +0300

    add

--- app/Http/Livewire/Pages/Cases/Admin/Add.php
+++ app/Http/Livewire/Pages/Cases/Admin/Add.php
@@ -26,7 +26,6 @@ class Add extends Component
             'description' => $this->description,
             'target' => $this->target,
             'received_price' => $this->received_price,
-
         ];
         $this->alert('success', 'تمت الاضافة', [
             'position' => 'top',
