2514a9609daa7e754cb4c6fa93ca065b50f4566e
Ahmed <mr.darkboss1@gmail.com>
2022-05-17 01:39:22 +0300

    A

--- resources/views/livewire/components/case/card.blade.php
+++ resources/views/livewire/components/case/card.blade.php
@@ -3,13 +3,14 @@
         class="rounded-xl border-2 border-primary-100 hover:shadow-lg hover:border-primary-500 w-96 h-auto group duration-300">
         {{-- case Image --}}
         <div class="pt-3 px-3">
-            {{-- <img class="rounded-t-lg" src="{{ asset($item->image_path) }}" alt="" /> --}}
+            {{-- <img class="rounded-t-lg" src="{{ asset($item->image_path) }}"
+            alt="" /> --}}
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI7Vm3bWFcLUw9o1XiIVwWv3Vcuitvy9GOCg&usqp=CAU"
                 class=" w-full rounded-lg ">
         </div>
         {{-- title + more info --}}
         <div class="p-6">
-            <h5 class="text-center font-bold group-hover:text-primary-500 text-3xl mb-2"> {{$event->title }}</h5>
+            <h5 class="text-center font-bold group-hover:text-primary-500 text-3xl mb-2"> {{ $event->title }}</h5>
             <div class=" xs:28">
                 <p class="text-gray-900 text-medium mb-4">
                     {{ $event->getLimit('description') }}
@@ -54,33 +55,33 @@
                     </div>
                     <div class="text-right mt-2">
                         @admin
-                        <div x-data="{ open: false }">
-                            <button wire:click="confirm({{ $event->id }})" class="mx-2">
-                                <i
-                                    class="text-red-400 fa-solid fa-trash text-xl hover:scale-110 duration-200 hover:text-red-600"></i>
+                            <div x-data="{ open: false }">
+                                <button wire:click="confirm({{ $event->id }})" class="mx-2">
+                                    <i
+                                        class="text-red-400 fa-solid fa-trash text-xl hover:scale-110 duration-200 hover:text-red-600"></i>
 
-                            </button>
-                            <button wire:click="$set('case_id', {{  $event->id }})" class="mx-2"
-                                type="button" data-modal-toggle="case-modal">
-                                <i
-                                    class="text-primary-400 fa-solid fa-pen-to-square text-xl hover:text-primary-600"></i>
-                            </button>
-                            @if ($event->received_price != $event->target)
-                            <button @click="open = ! open" class="mx-2">
-                                <i
-                                    class="text-red-400 fa-solid fa-plus text-xl hover:scale-110 duration-200 hover:text-red-600"></i>
-                            </button>
-                                <div x-show="open" @click.outside="open = false">
-                                    <input type="text" wire:model="received_price"
-                                        class="ml-4 bg-gray-50 border border-primary-700 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-28 pl-10 p-2.5"
-                                        placeholder="" required="">
-                                    <button type="submit" wire:click="add_price ({{ $event->id }})"
-                                        class=" mt-4 inline-flex items-center py-2 px-2 ml-2 text-sm font-medium text-white bg-primary-600 rounded-lg border border-primary-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                                        اضافة</button>
-                                </div>
-                            @endif
-                        </div>
-                    @endadmin
+                                </button>
+                                <button wire:click="$set('case_id', {{ $event->id }})" class="mx-2" type="button"
+                                    data-modal-toggle="case-modal">
+                                    <i
+                                        class="text-primary-400 fa-solid fa-pen-to-square text-xl hover:text-primary-600"></i>
+                                </button>
+                                @if($event->received_price != $event->target)
+                                    <button @click="open = ! open" class="mx-2">
+                                        <i
+                                            class="text-red-400 fa-solid fa-plus text-xl hover:scale-110 duration-200 hover:text-red-600"></i>
+                                    </button>
+                                    <div x-show="open" @click.outside="open = false">
+                                        <input type="text" wire:model="received_price"
+                                            class="ml-4 bg-gray-50 border border-primary-700 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-28 pl-10 p-2.5"
+                                            placeholder="" required="">
+                                        <button type="submit" wire:click="add_price ({{ $event->id }})"
+                                            class=" mt-4 inline-flex items-center py-2 px-2 ml-2 text-sm font-medium text-white bg-primary-600 rounded-lg border border-primary-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                                            اضافة</button>
+                                    </div>
+                                @endif
+                            </div>
+                        @endadmin
                     </div>
                 </div>
 
@@ -103,7 +104,8 @@
                     </button>
                 </div>
                 <!-- Modal body -->
-                @livewire('pages.cases.admin.edit', ['case_id' => $event->id], key($event_id))
+                <blade
+                    livewire|(%26%2339%3Bpages.cases.admin.edit%26%2339%3B%2C%20%5B%26%2339%3Bcase_id%26%2339%3B%20%3D%3E%20%24event-%3Eid%5D%2C%20key(%24event_id))%0D>
             </div>
         </div>
     </div>
