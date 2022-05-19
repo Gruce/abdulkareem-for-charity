ee91a2da480e1d20a2011609afc497c38aae23fd
Hussein Najah <98782046+i1Zeus@users.noreply.github.com>
2022-05-19 19:21:11 +0300

    1

--- resources/views/livewire/pages/about/main.blade.php
+++ resources/views/livewire/pages/about/main.blade.php
@@ -1,29 +1,4 @@
 <div>
-    {{-- <div class="h-screen grid md:grid-cols-10 sm:grid-cols-1 sm:grid-rows-6">
-        <div class="sm:col-span-6 sm:grid-rows-4 mt-10 md:mt-0">
-
-            <h1 class="text-3xl md:text-5xl font-Alhurra text-center font-bold text-primary-600"> صندوق الأستاذ
-                عبدالكريم
-            </h1>
-            <p class="mt-10 text-xl text-gray-700 font-Alhurrareg px-1 md:px-9">"صـندوق الأستاذ عبد الكريم الخيري" هو
-                مبادرة ونشاط طلابي
-                بحت ينطلق من
-                طلبة كلية علوم الحاسوب وتكنلوجيا المعلومات. <br>
-
-                الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق للطلبة ذوي الدخل دون المحدود
-                وبقية الاعمال الخيرية. <br>
-
-                اساس عمل الصندوق قائم على اشتراكات شهرية لكل طالب او منتسب داخل الكلية، تبدئ الاشتراكات من مبلغ (٢٠٠٠)
-                ألفين دينار عراقي او اكثر،<br>
-                اساس عمل الصندوق سيتم جمع لجان من الطلبة من مختلف المراحل مع ممثلي المراحل للأشراف على : <br>
-
-                1- جمع المبالغ <br>
-                2- أمانة الصندوق <br>
-                3- الاعمال التي تُصرف فيها مبالغ الصندوق<br>
-            </p>
-        </div>
-    </div> --}}
-
     <div class="flex flex-col px-16">
 
         <div class="basis-1/4 grid grid-cols-12 justify-between">
@@ -143,17 +118,6 @@
                 {{-- Cards --}}
                 <div class="grid grid-cols-4 gap-8 ">
                     @foreach ($managers as $team)
-                    {{-- <div
-                        class="p-8 w-72 h-44  rounded-2xl hover:bg-white mt-14 mb-4 bg-white transition duration-300 ease-in-out hover:scale-105  border border-gray-600">
-                        <div class="-mt-8 -translate-y-1/2 transform rounded-full">
-                            <img src="{{ $item->photo }}" class="mx-auto h-32 rounded-full" />
-                        </div>
-                        <div class="text-center -mt-14">
-                            <h3 class="text-3xl font-bold mb-4">{{ $item->name }}</h3>
-                            <span class="text-sm">{{ $item->stage }}</span>
-                        </div>
-                    </div> --}}
-
                     <livewire:components.about.card :team="$team" key="{{now()}}" />
                     @endforeach
                 </div>
@@ -168,17 +132,6 @@
                 {{-- Cards --}}
                 <div class="grid grid-cols-4 gap-8 ">
                     @foreach ($developers as $team)
-                    <!-- <div
-                        class="p-8 w-72 h-44  rounded-2xl hover:bg-white mt-14 mb-4 bg-white transition duration-300 ease-in-out hover:scale-105  border border-gray-600">
-                        <div class="-mt-8 -translate-y-1/2 transform rounded-full">
-                            <img src="{{$team->photo}}" class="mx-auto h-32 rounded-full" />
-                        </div>
-                        <div class="text-center -mt-14">
-                            <h3 class="text-3xl font-bold mb-4">{{$team->name}}</h3>
-                            <span class="text-sm">{{$team->stage}}</span>
-                        </div>
-                    </div> -->
-
                     <livewire:components.about.card :team="$team" key="{{now()}}" />
                     @endforeach
                 </div>
