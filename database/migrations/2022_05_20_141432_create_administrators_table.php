8051678e2bdb84a42da85d67a8f584fa24dffe22
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-05-20 14:18:39 +0300

    Update 2022_05_20_141432_create_administrators_table.php

--- database/migrations/2022_05_20_141432_create_administrators_table.php
+++ database/migrations/2022_05_20_141432_create_administrators_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone_number')->nullable();
-            //$table->integer('type')->default(1); // developer = 1 : manager = 2
+            //$table->integer('type')->default(1); // ** = 1 : ** = 2
             $table->string('photo');
             $table->timestamps();
         });
