9e82b693951557e7da0f2ffc91fe515a3d76baaf
Mohammed Jawad <jawad6686@gmail.com>
2022-05-21 18:10:36 +0300

    add

---
+++ app/Http/Livewire/Pages/Home/Info.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Home;
+
+use App\Models\{
+    Event,
+    Share,
+    User,
+};
+use Livewire\Component;
+
+class Info extends Component
+{
+    
+
+    public function render()
+    {
+        $users = User::all()->count();
+        $share = Share::all()->sum('share');
+        $event = Event::whereColumn('received_price', 'target')->count();
+        $payments = Event::sum('received_price');
+        $total = Share::where('state', true)->sum('share') * 2000;
+        $current_price = $total - $payments;
+
+
+
+        $menu = new Menu([
+            [
+                'info' => 'المبلغ الكلي',
+                'value' => $total,
+
+            ],
+            [
+                'info' => 'المبلغ الحالي',
+                'value' => $current_price,
+
+            ],
+            [
+                'info' => 'مصروفات',
+                'value' => $payments,
+
+            ],
+
+            [
+                'info' => 'متبرعين',
+                'value' => $users,
+
+            ],
+            [
+                'info' => 'حالات معالجة',
+                'value' => $event,
+
+            ],
+            [
+                'info' => 'اسهم',
+                'value' =>  $share,
+
+            ],
+
+
+        ]);
+        
+        return view('livewire.pages.home.info', [
+            'menu' => $menu,
+        ]);
+    }
+}
+
+class Menu
+{
+    public $items;
+
+    function __construct($items = [])
+    {
+        // Menu Generation
+        foreach ($items as $item) $this->items[] = new MenuItem($item);
+    }
+}
+
+class MenuItem
+{
+    public $info;
+    public $value;
+
+
+    public function __construct($data)
+    {
+        $this->info = $data['info'];
+        $this->value = $data['value'];
+    }
+}
---
+++ resources/views/livewire/pages/home/info.blade.php
@@ -0,0 +1,33 @@
+<div>
+    @if($state)
+
+    {{-- activites --}}
+    @forelse($activities->items as $item)
+    <div class="p-4 bg-white rounded-lg border border-gray-200 shadow-md xl:m-5 m-1 ml-10 xl:ml-0">
+        <div class="flex flex-col p-2 text-center xl:mx-10">
+            <i class="fa-solid fa-{{ $item->value }} text-7xl text-primary-500"></i>
+
+            <span class="text-xl xl:text-2xl font-bold mt-2 mx-auto">
+                {{ $item->info }}
+            </span>
+        </div>
+    </div>
+    @empty
+    @endforelse
+    @else
+    {{-- info --}}
+    @forelse($menu->items as $item)
+    <div class="flex flex-col p-2 text-center mx-3 xl:mx-20 mb-4 xl:mb-10">
+        <span class="text-sm md:text-lg xl:text-4xl font-bold font-noto">
+            {{ $item->value }}
+        </span>
+
+        <span class="text-xs md:text-base xl:text-2xl">
+            {{ $item->info }}
+        </span>
+    </div>
+    @empty
+    @endforelse
+
+    @endif
+</div>
