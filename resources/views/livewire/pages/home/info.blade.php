4927ec05194c73803cc853e8f1598ec287c45f74
Mohammed Jawad <jawad6686@gmail.com>
2022-05-21 20:12:58 +0300

    Update info.blade.php

--- resources/views/livewire/pages/home/info.blade.php
+++ resources/views/livewire/pages/home/info.blade.php
@@ -1,4 +1,4 @@
-<section class="p-6  text-gray-800">
+<section class="p-6  text-secondary-800">
     <div class="container mx-auto grid justify-center grid-cols-2 text-center lg:grid-cols-3">
         @forelse($menu->items as $item)
         <div class="flex flex-col justify-start m-2 lg:m-6">
@@ -6,7 +6,5 @@
             <p class="text-sm sm:text-base">{{ $item->info }}</p>
         </div>
         @endforeach
-
-
     </div>
 </section>
