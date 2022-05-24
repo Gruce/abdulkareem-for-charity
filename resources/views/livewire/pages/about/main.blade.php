53cfb814336412012200ec7075632c353e183182
Mohammed Jawad <jawad6686@gmail.com>
2022-05-24 17:34:43 +0300

    add

--- resources/views/livewire/pages/about/main.blade.php
+++ resources/views/livewire/pages/about/main.blade.php
@@ -1,20 +1,22 @@
 <div>
-    <div class="flex flex-col md:px-16">
+    <div class="flex flex-col  container mx-auto">
 
         {{-- Intro --}}
         <div class="basis-1/4 grid md:grid-cols-12 justify-between">
 
             <!-- Text -->
-            <div class="md:col-span-7   text-center">
+            <div class="md:col-span-7 text-center md:mt-20">
                 <h1 class="text-secondary-900 text-4xl md:text-6xl font-bold ">
                     صندوق الكرم
                 </h1>
-                <p class="leading-10 mt-6 md:mt-10 text-xl text-gray-700 font-Alhurrareg px-1 md:px-9 text-center md:text-right">
+                <p
+                    class="leading-10 mt-6 md:mt-10 text-base md:text-xl text-gray-700 font-Alhurrareg px-2 md:px-12   ">
                     هو مبادرة ونشاط طلابي بحت ينطلق من طلبة كلية علوم الحاسوب
-                    وتكنلوجيا المعلومات. الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في صرف الصندوق
+                    وتكنلوجيا المعلومات في جامعة البصرة. الهدف منه رعاية الاعمال الخيرية داخل الكلية وتكون الاولوية في
+                    صرف الصندوق
                     للطلبة
                     ذوي الدخل دون المحدود وبقية الاعمال الخيرية.
-                    <br>
+                    
                     سمي بأسم صندوق الكرم نسبة الى الأستاذ الراحل عبد الكريم حسين
                 </p>
 
@@ -33,18 +35,21 @@
             <div class="basis-1/2 md:col-span-5 ">
                 <img src="/img/About2.png" class="w-3/4 h-auto m-auto">
             </div>
-        
+
             <!-- Text -->
-            <div class="basis-1/2 md:col-span-7 mt-4 md:mt-20   text-center">
+            <div class="basis-1/2 md:col-span-7 mt-4 md:mt-40   text-center">
                 <h1 class="text-secondary-900 text-2xl md:text-6xl font-bold ">
                     من هو الأستاذ عبدالكريم
                 </h1>
-                <p class="leading-10 mt-10 text-xl text-gray-700 font-Alhurrareg px-1 md:px-9 text-right">
-
+                <p class="leading-10 mt-10 text-base md:text-xl text-gray-700 font-Alhurrareg px-2 md:px-9 text-center">
+                    الأستاذ عبد الكريم حسين السوداني (1974 - 2021) تدريسي في كلية علوم الحاسوب وتكنلوجيا المعـلومات
+                    وشغل منصب مدير وحدة شؤون الطلبة سابقا
+                    عُرف عنه فعل الخير والمشاركة في النشاطات الخيرية والاهتمام بطلبته وتشجيعهم فأحبه طلبته و ارادوا ابقاء ذكر
+                    أستاذهم حتى بعد ان تَوَفَّاه الله لذا بدأوا بأنشاء مجموعة من الاعمال الخيرية داخل الكلية وربطها بأسم الأستاذ و أحد هذه الاعمال هذا الصندوق الخيري.
                 </p>
-        
+
             </div>
-        
+
         </div>
 
 
