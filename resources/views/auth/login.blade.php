b0108adeb1cb1905fd9b29a608b76f14a906bcf2
Ahmed <mr.darkboss1@gmail.com>
2022-06-03 21:07:08 +0300

    profile img

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,12 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <img src="{{ asset('/img/logo.png') }}" class="mb-4 h-32">
+            <img src="{{ asset('/img/logo1.png') }}" class="mb-4 h-56 md:h-72">
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
 
-        @if (session('status'))
+        @if(session('status'))
             <div class="mb-4 text-sm font-medium text-green-600">
                 {{ session('status') }}
             </div>
@@ -16,19 +16,26 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('البريد الأالكتروني') }}" />
-                <x-jet-input id="email" class="block w-full mt-1 border-gray-300 shadow-sm focus:border-green-500 focus:bg-white focus:ring-0" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-label for="email"
+                    value="{{ __('البريد الأالكتروني') }}" />
+                <x-jet-input id="email"
+                    class="block w-full mt-1 border-gray-300 shadow-sm focus:border-green-500 focus:bg-white focus:ring-0"
+                    type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('كلمة السر') }}" />
-                <x-jet-input id="password" class="block w-full mt-1 border-gray-300 shadow-sm focus:border-green-500 focus:bg-white focus:ring-0" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-label for="password"
+                    value="{{ __('كلمة السر') }}" />
+                <x-jet-input id="password"
+                    class="block w-full mt-1 border-gray-300 shadow-sm focus:border-green-500 focus:bg-white focus:ring-0"
+                    type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600 px-2">{{ __('تذكرني') }}</span>
+                    <span
+                        class="ml-2 text-sm text-gray-600 px-2">{{ __('تذكرني') }}</span>
                 </label>
             </div>
 
@@ -37,8 +44,9 @@
                     {{ __('تسجيل دخول') }}
                 </x-jet-button>
 
-                @if (Route::has('password.request'))
-                    <a class="text-sm text-gray-600 underline hover:text-gray-900" href="{{ route('password.request') }}">
+                @if(Route::has('password.request'))
+                    <a class="text-sm text-gray-600 underline hover:text-gray-900"
+                        href="{{ route('password.request') }}">
                         {{ __('نسيت كلمة السر ؟') }}
                     </a>
                 @endif
