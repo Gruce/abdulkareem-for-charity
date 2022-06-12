68d7fad27e03b8843cda250f004401cedf5d338a
Mohammed Jawad <jawad6686@gmail.com>
2022-06-12 17:53:19 +0300

    Update navbar.blade.php

--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -14,12 +14,11 @@
             @auth
 
             <div class="flex justify-around items-center md:order-2 md:-mt-4 @guest hidden @endguest ml-2">
-              @admin
-                <div
-                    class="hidden   md:flex justify-around   ml-10  text-gray-500 text-base text-center ">
+                @admin
+                <div class="hidden   md:flex justify-around   ml-10  text-gray-500 text-base text-center ">
 
                     <div class="flex flex-col mx-2 border-b border-gray-200">
-                        <span>@money($total, 'IQD')  </span>
+                        <span>@money($total, 'IQD') </span>
                         <span class="text-2xs -mt-2">الكلي</span>
                     </div>
                     <div class="flex flex-col mx-2 border-b border-gray-200">
